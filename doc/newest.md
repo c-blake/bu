69d2dde7a30a0919a577cbdeaba40932e8e48912
Charles Blake <charlechaud@gmail.com>
2022-10-10 09:06:00 -0400

    Import `newest` from `cligen/examples`; Try to explain in `doc/newest.md`

---
+++ doc/newest.md
@@ -0,0 +1,45 @@
+Motivation
+----------
+This is (mostly) a convenience program for something I often want to know.
+
+Usage
+-----
+```
+  printNewest [optional-params] [paths: string...]
+
+Echo ended by outEnd the <= n newest files in file time order {-}[bamcv] for
+Birth, Access, Mod, Ctime, Version=max(MC); { - | CAPITAL means oldest }.
+
+Examined files = UNION of paths + optional delim-delimited input file (stdin
+if "-"|if "" & stdin is not a terminal), maybe recursed as roots.
+
+E.g. to echo the 3 oldest regular files by m-time under the CWD:
+  newest -t-m -n3 -r0 .
+
+Options:
+  -n=, --n=       int           1     number of 'newest' files
+  -t=, --time=    string        "m"   timestamp to compare ({-}[bamcv]*)
+  -r=, --recurse= int           1     recurse n-levels on dirs; 0:unlimited
+  -c, --chase     bool          false chase symlinks to dirs in recursion
+  -D, --Deref     bool          false dereference symlinks for file times
+  -k=, --kinds=   set(FileKind) file  i-node type like find(1): [fdlbcps]
+  -q, --quiet     bool          false suppress file access errors
+  -x, --xdev      bool          false block recursion across device boundaries
+  -o=, --outEnd=  string        "\n"  output record terminator
+  -f=, --file=    string        ""    optional input ("-"|!tty=stdin)
+  -d=, --delim=   char          '\n'  input file record delimiter
+  -e, --eof0      bool          false set eof0
+```
+
+Related Work
+------------
+`find -printf` does not support the new-ish Linux b-time.  Even if it did one
+would need to pipe its output to something like `topn 3` (which also does not
+exist) that maintained a heap sorted by the desired time (in the desired order)
+to be as memory efficient.  `sort` is highly wasteful for this use case.
+
+This also uses my `cligen/dents` tree walker to be faster than `find` (much
+faster with `-d:batch` & a custom kernel module) on Linux.  GNU `find` worries
+about arbitrary FS depth while I've never seen a (non-artificial) depth > 30.
+Common cases should not suffer from pathologies.  Default open fd limits that
+hail from 1970s memory costs are are already pretty dumb.
---
+++ newest.nim
@@ -0,0 +1,55 @@
+import std/[heapqueue, posix], cligen, cligen/[osUt, posixUt, dents, statx]
+
+type TimePath = tuple[tm: int64, path: string]
+
+proc doStat(dfd: cint, path: string; nmAt: int, st: var Statx; Deref,
+            quiet: bool): bool {.inline.} =
+  if Deref:     # lstat if no deref requested or as fallback if stat fails.
+    if statxat(dfd, path[nmAt..^1].cstring, st, 0) == 0: return true
+    if lstatxat(dfd, path[nmAt..^1].cstring, st, 0) == 0: return true
+  elif lstatxat(dfd, path[nmAt..^1].cstring, st, 0) == 0: return true
+  if not quiet: stderr.write "newest: \"", path, "\" ", strerror(errno), '\n'
+
+proc printNewest*(n=1, time="m", recurse=1, chase=false, Deref=false,
+                  kinds={fkFile}, quiet=false, xdev=false, outEnd="\n",
+                  file="", delim='\n', eof0=false, paths: seq[string]) =
+  ## Echo ended by *outEnd* <= *n* newest files in file *time* order
+  ## `{-}[bamcv]` for Birth, Access, Mod, Ctime, Version=max(MC); { `-` | CAPITAL
+  ## means ***oldest*** }.  Examined files = UNION of *paths* + optional
+  ## *delim*-delimited input *file* ( ``stdin`` if `"-"`|if `""` & ``stdin`` is
+  ## not a terminal ), **maybe recursed** as roots.  E.g. to echo the 3 oldest
+  ## regular files by m-time under the CWD: ``newest -n3 -t-m -r0 .``.
+  let err = if quiet: nil else: stderr
+  let tO = fileTimeParse(time)                  #- or CAPITAL=oldest
+  let it = both(paths, fileStrings(file, delim))
+  var q  = initHeapQueue[TimePath]()            # min-heap with q[0]=min
+  for root in it():
+    if root.len == 0: continue                  # skip any improper inputs
+    forPath(root, recurse, false, chase, xdev, eof0, err,
+            depth, path, nmAt, ino, dt, lst, dfd, dst, did):
+      if dt != DT_UNKNOWN:                      # unknown here => disappeared
+        if (dt==DT_LNK and Deref and doStat(dfd,path,nmAt,lst,Deref,quiet)) or
+           lst.stx_nlink != 0 or doStat(dfd,path,nmAt,lst,Deref,quiet):
+          if lst.stx_mode.match(kinds):
+            let tp = (fileTime(lst, tO.tim, tO.dir), path)
+            if q.len < n  : q.push(tp)
+            elif tp > q[0]: discard q.replace(tp)
+    do: discard
+    do: discard
+    do: recFailDefault("newest", path)
+  while q.len > 0:                              # q now has top n times; Print
+    stdout.write q.pop().path, outEnd           #..in the specified-time order.
+
+when isMainModule:  # Exercise this with an actually useful CLI wrapper.
+  dispatch printNewest,
+           help = { "n"      : "number of 'newest' files",
+                    "time"   : "timestamp to compare ({-}[bamcv]\\*)",
+                    "recurse": "recurse n-levels on dirs; 0:unlimited",
+                    "chase"  : "chase symlinks to dirs in recursion",
+                    "xdev"   : "block recursion across device boundaries",
+                    "Deref"  : "dereference symlinks for file times",
+                    "kinds"  : "i-node type like find(1): [fdlbcps]",
+                    "quiet"  : "suppress file access errors",
+                    "outEnd" : "output record terminator",
+                    "file"   : "optional input (\"-\"|!tty=stdin)",
+                    "delim"  : "input file record delimiter" }
