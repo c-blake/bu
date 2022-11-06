86d5316bc214d540cbb39cdda3d15324c0bc7ef2
Charles Blake <charlechaud@gmail.com>
2022-11-06 14:52:37 -0500

    Use new -t feature of mnsd&manually edit to reduce from width 133 -> 106 (The -h in ru -hut will make re-run mxR be MiB not KiB, though. Oh well.)

--- tests/strench.sh
+++ tests/strench.sh
@@ -10,32 +10,34 @@ $c -static -Os $t/true.c -o $t/true && rm $t/true.c || exit 3
 
 tm() { # Now measure dispatch overhead
     (for i in {1..20}; do $@ < $t/w; done) |&
-      sort -g | head -n5 | mnsd         # NOTE: mnsd.nim is in adix/util
+      sort -g | head -n5 | mnsd -t,     # NOTE: mnsd.nim is in adix/util
 }
-(tm env -i PATH=$PATH ru -t stripe 1    # stripe 1 w/1 envar
- tm ru -t stripe 1                      # stripe 1 w/~50 envars
- tm ru -t xargs -n1 $t/true             # xargs -n1
- tm ru -t parallel -j1)|                # parallel -j1
-    align -dw                           # make table easier to read
+(tm env -i PATH=$PATH ru -hut stripe 1  # stripe 1 w/1 envar
+ tm ru -hut stripe 1                    # stripe 1 w/~50 envars
+ tm ru -hut xargs -n1 $t/true           # xargs -n1
+ tm ru -hut parallel -j1)|              # parallel -j1
+    align                               # make table easier to read
 
-# You will probably want a wide terminal window for the below.  Sorry.
-# i7-6700k w/HT off taskset 0x3 chrt -r 99 full meltdown-spectre mitigations                                           RATIO
-# TM 0.041408 +- 0.000070 wall 0.03259 +- 0.00048 usr 0.01003 +- 0.00050 sys 102.94  +- 0.20  % 2424  +- 20 mxRS       1
-# TM 0.04484  +- 0.00010  wall 0.03489 +- 0.00041 usr 0.01105 +- 0.00040 sys 102.460 +- 0.046 % 2481  +- 14 mxRS       1.08288
-# TM 0.15775  +- 0.00040  wall 0.11863 +- 0.00098 usr 0.0506  +- 0.0014  sys 107.28  +- 0.27  % 1920  +- 28 mxRS       3.80965
-# TM 2.5313   +- 0.0057   wall 1.257   +- 0.017   usr 1.841   +- 0.014   sys 122.380 +- 0.059 % 19290 +- 15 mxRS       61.1307
+# You'll want a wide terminal window for below. Sorry. Manually cleaned up some.
+# i7-6700k w/HT off taskset 0x3 chrt -r 99 full meltdown-spectre mitigations
+# wall                 usr                sys                %                mxRS         /first
+# 0.041408 +- 0.000070 0.03259 +- 0.00048 0.01003 +- 0.00050 102.94  +- 0.20  2424  +- 20  1
+# 0.04484  +- 0.00010  0.03489 +- 0.00041 0.01105 +- 0.00040 102.460 +- 0.046 2481  +- 14  1.08288
+# 0.15775  +- 0.00040  0.11863 +- 0.00098 0.0506  +- 0.0014  107.28  +- 0.27  1920  +- 28  3.80965
+# 2.5313   +- 0.0057   1.257   +- 0.017   1.841   +- 0.014   122.380 +- 0.059 19290 +- 15  61.1307
 # (0.04484-0.041408)/49e3*1e9 =~ 70.0 ns/envar
 #
-# i7-1270P w/taskset 0x7 chrt -r 99 full meltdown-spectre mitigations                                                  RATIO
-# TM 0.022668 +- 0.000057 wall 0.01666 +- 0.00030 usr (7.38   +- 0.29)e-03 sys 106.040 +- 0.061 % 2453.60 +- 0.88 mxRS 1
-# TM 0.02523  +- 0.00011  wall 0.01907 +- 0.00025 usr (7.57   +- 0.27)e-03 sys 105.64  +- 0.20  % 2485    +- 31   mxRS 1.11302
-# TM 0.07492  +- 0.00020  wall 0.05964 +- 0.00050 usr 0.02344 +- 0.00037   sys 110.880 +- 0.052 % 1970    +- 31   mxRS 3.3051
-# TM 1.649    +- 0.045    wall 0.764   +- 0.011   usr 1.233   +- 0.085     sys 120.9   +- 1.6   % 19322   +- 12   mxRS 72.7457
+# i7-1270P w/taskset 0x7 chrt -r 99 full meltdown-spectre mitigations
+# wall                 usr                sys                  %                mxRS            /first
+# 0.022668 +- 0.000057 0.01666 +- 0.00030 (7.38   +- 0.29)e-03 106.040 +- 0.061 2453.60 +- 0.88 1
+# 0.02523  +- 0.00011  0.01907 +- 0.00025 (7.57   +- 0.27)e-03 105.64  +- 0.20  2485    +- 31   1.1130
+# 0.07492  +- 0.00020  0.05964 +- 0.00050 0.02344 +- 0.00037   110.880 +- 0.052 1970    +- 31   3.3051
+# 1.649    +- 0.045    0.764   +- 0.011   1.233   +- 0.085     120.9   +- 1.6   19322   +- 12   72.746
 # (0.02523-0.022668)/49e3*1e9 =~ 52 ns/envar
 #
 # External facts: Run-to-run deltas have means within 0.2-4 sigma of each other.
-# For example `paste` two together (with manually computed ratio column above
-# included) & pipe to awk '{print ($3-$25)/($5**2+$27**2)**.5}', I got 0.670551
+# For example `paste` two together (WITH manually computed ratio column above
+# included) & pipe to awk '{print ($2-$18)/($4**2+$20**2)**.5}', I got 0.670551
 # -1.87678 2.26593 -0.263955: quite decent considering system noise has a heavy
 # tail with slow convergence to Normal.  (align -1 can provide columns for awk).
 # Hence mean,sdev(best 5 of 20 trials) leads to locally reproduced estimates of
@@ -50,11 +52,12 @@ tm() { # Now measure dispatch overhead
 #   - stripe overhead is low enough that usually neglected things
 #     (like environ.len, argc, etc.) start to matter a lot.
 #
-# One can also study other scenarios, like failed execs on i7-1270P:                                                          RATIO
-# TM 0.01507  +- 0.00016   wall (9.44   +- 0.38)e-03 usr (6.42    +-  0.26)e-03 sys 105.22  +- 0.18  %  2375    +-   24  mxRS 1
-# TM 0.017871 +- 0.000026  wall 0.01210 +- 0.00031   usr (6.61    +-  0.30)e-03 sys 104.640 +- 0.088 %  2344    +-   21  mxRS 1.18587
-# TM (3.31    +- 0.15)e-04 wall (4.38   +- 0.44)e-04 usr 0.000000 sys 138       +-  23      %  1960  +- 16      mxRS          0
-# TM 1.836    +- 0.010     wall 0.794   +- 0.014     usr 1.4731   +-  0.0056    sys 123.48  +- 0.11  %  19328.0 +-   2.3 mxRS 121.831
+# One can also study other scenarios, like failed execs on i7-1270P:
+# wall                  usr                  sys                    %                mxRS           /first
+# 0.01507  +- 0.00016   (9.44   +- 0.38)e-03 (6.42    +-  0.26)e-03 105.22  +- 0.18  2375    +- 24  1
+# 0.017871 +- 0.000026  0.01210 +- 0.00031   (6.61    +-  0.30)e-03 104.640 +- 0.088 2344    +- 21  1.1859
+# (3.31    +- 0.15)e-04 (4.38   +- 0.44)e-04 0.000000               138     +- 23    1960    +- 16  0
+# 1.836    +- 0.010     0.794   +- 0.014     1.4731   +-  0.0056    123.48  +- 0.11  19328.0 +- 2.3 121.83
 # (xargs here is only failing *1* exec, while the other two fail 1000...)
 #
 # From the delta of the successful exec case, one can read off costs of making
