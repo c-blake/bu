063572e09e3c2ca379f1d27ea44dff073eb13fac
Charles Blake <charlechaud@gmail.com>
2023-01-05 12:02:53 -0500

    Big re-factor into a template `bu/emin.eMin` & much simpler CLI `tim`. sigma -> dist rename & new ability to `import bu/emin` for proc/etc. timing are the most user-visible changes.
    Also include better code comments about alternative ideas/definitions
    in the new bu/emin.nim.

---
+++ bu/emin.nim
@@ -0,0 +1,40 @@
+import std/[stats, math, algorithm]
+
+type MinEst* = object               ## Holds state for 2-stage minimum estimate
+  r1*, r2*, r*: seq[float]          ## stage1, stage2, combined data
+  s1*, s2*, s*: RunningStat         ## std/stats summaries of r1/r2/r
+  est1*, err1*, est2*, err2*: float ## stage1 estimates of 2 subsamples
+  apart*: float                     ## statistical distance between 2 subsamples
+  measured*: bool                   ## flag indicating successful measurement
+  est*, err*: float                 ## Successfully estimated minimum & its err
+
+proc distance(s1, s2: RunningStat; r1, r2: seq[float]): float = # Could be KS-
+  abs(s1.mean - s2.mean) / sqrt(s1.variance + s2.variance)      # test(near min)
+
+proc stage2(s: RunningStat, r: seq[float]): (float, float) = # Stage 2 estimator
+  (s.mean, s.standardDeviation) #TODO stg2==stg1 now; (r[0], (r[1]-r[0])/3.0)?
+
+template measureSortSummarize(rx, sx, sample1) =
+  for it in 1..n:                       # Measure n trials
+    rx.add (block: sample1)
+  rx.sort                               # Sort
+  sx.push rx[0..<best]                  # Summarize
+
+template eMin*(n=10, best=3, dist=7.5, sample1): untyped =
+  ## This template takes as its final parameter any block of Nim code that
+  ## produces a single `float`, probably a delta time.  `doc/tim.md` explains
+  ## while `bu/tim.nim` is a fully worked example to time programs reliably.
+  var result: MinEst
+  measureSortSummarize(result.r1, result.s1, sample1)
+  measureSortSummarize(result.r2, result.s2, sample1)
+  result.apart = distance(result.s1, result.s2, result.r1, result.r2)
+  if result.apart < dist:               # Stable sampling distro.
+    result.r = result.r1 & result.r2    # Merge all data to refine estimate
+    result.r.sort
+    result.s.push result.r[0..<best]
+    (result.est, result.err) = stage2(result.s, result.r)
+    result.measured = true              # !measured => (est,err)==(0.0,0.0)
+  result
+#IDEAS:
+# - Do k-sample Anderson-Darling (min-tail weighted) for coherence
+# - User-parameterize both stages w/various ideas; Cross-deployment Experiment
--- tim.nim
+++ tim.nim
@@ -1,45 +1,37 @@
 when not declared(addfloat): import std/[syncio, formatfloat]
-import std/[os, times, algorithm, stats, math, strformat], cligen, cligen/strUt
+import std/[os, times, strformat], cligen, cligen/strUt, bu/emin
 
-proc run(cmd: string, runs=10): seq[float] =
-  for trial in 1..runs:
-    let t0 = epochTime()
-    if execShellCmd(cmd) != 0: quit "could not run \"" & cmd & "\"", 2
-    result.add epochTime() - t0
+proc sample1(cmd: string): float =
+  let t0 = epochTime()
+  if execShellCmd(cmd) != 0:
+    quit "could not run \"" & cmd & "\"", 2
+  epochTime() - t0
 
-proc me(s: RunningStat): (float, float) = (s.mean, s.standardDeviation)
-
-proc tim(best=3, runs=10, sigma=7.5, write="", cmds: seq[string]) =
-  ## Run shell commands (maybe w/escape/quoting) 2*R times.  Finds mean,"err"
-  ## of the `best` `runs` twice and, if stable at `sigma`-level, merge results
-  ## (reporting mean,"err" of the `best` of all runs).  `doc/tim.md` says more.
-  if runs<best: raise newException(HelpError, "Need runs >= best; Full ${HELP}")
-  if cmds.len==0: raise newException(HelpError, "Need cmds; Full ${HELP}")
+proc tim(n=10, best=3, dist=7.5, write="", cmds: seq[string]) =
+  ## Run shell cmds (maybe w/escape|quoting) `2*n` times.  Finds mean,"err" of
+  ## the `best` twice and, if stable at level `dist`, merge results for a final
+  ## time & error estimate.  `doc/tim.md` explains.
+  if n < best:
+    raise newException(HelpError, "Need n >= best; Full ${HELP}")
+  if cmds.len == 0:
+    raise newException(HelpError, "Need cmds; Full ${HELP}")
   let f = if write.len > 0: open(write, fmWrite) else: nil
   for cmd in cmds:
-    var s1, s2, s: RunningStat
-    var r1 = run(cmd, runs)
-    var r2 = run(cmd, runs)
-    r1.sort; s1.push r1[0..<best]
-    r2.sort; s2.push r2[0..<best]
-    let (m1, e1) = s1.me
-    let (m2, e2) = s2.me
-    let sigmas = abs(m1 - m2) / sqrt(e1*e1 + e2*e2)
-    if sigmas > sigma:
-      let sa = &"{sigmas:.1f}"
-      echo &"UNSTABLE; Mean,\"err\"(Best {best} of {runs}):\n\t",
-           fmtUncertain(m1, e1), "\n\t", fmtUncertain(m2, e2), "\n", &"""
-are {sa} sig apart.  taskset, chrt & if those fail fixing CPU freqs (either in
-OS|BIOS) may stabilize timing as can suspend/quit of competing work/browsers."""
-    else:       # Compute best of all runs & print mean,"err"
-      var r = r1 & r2; r.sort; s.push r[0..<best]
-      let (m,e) = s.me; echo fmtUncertain(m, e), "\t", cmd
-    if not f.isNil:
-      for t in r1: f.write $t,'\n'
-      for t in r2: f.write $t,'\n'
+    var e = eMin(n, best, dist, sample1(cmd))         # Get estimate w/error
+    if e.measured:
+      echo fmtUncertain(e.est, e.err),"\t",cmd        # Report if successful
+    else:
+      let sa = &"{e.apart:.2f}"                       # Informative failure
+      echo &"UNSTABLE; Mean,\"err\"(Best {best} of {n}) stage 1's:\n\t",
+        fmtUncertain(e.est1,e.err1),"\n\t",fmtUncertain(e.est2,e.err2),"\n",&"""
+are {sa} err apart.  taskset, chrt, fixing CPU freqs (either in OS|BIOS) may
+stabilize time sampling as can suspend/quit of competing work/browsers."""
+    if not f.isNil:                                   # Optionally log for..
+      for t in e.r1: f.write $t,'\n'                  #..further analysis.
+      for t in e.r2: f.write $t,'\n'
 
 when isMainModule: include cligen/mergeCfgEnv; dispatch tim, help={
+  "n"    : "number of outer trials; 1/2 total",
   "best" : "number of best times to average",
-  "runs" : "number of outer trials",
-  "sigma": "max distance to declare stability",
+  "dist" : "max distance to decide stable samples",
   "write": "also write times to this file"}
