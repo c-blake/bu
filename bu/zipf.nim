e1f3a420551eae3c7d0692692d68e8a19f3c63f9
Charles Blake <charlechaud@gmail.com>
2023-03-08 12:45:31 -0500

    Add a small state method & option;  (Also keys.setLen for 3X speed-up in that mode.)

--- bu/zipf.nim
+++ bu/zipf.nim
@@ -1,3 +1,12 @@
+## This samples from power-law Zipf distributions two ways: either explicit CDF
+## construction and using std/random.sample(openArray,cdf) or via the method of
+## Hormann, Derflinger: "Rejection-Inversion to Generate Variates from Monotone
+## Discrete Distributions" eeyore.wu-wien.ac.at/papers/96-04-04.wh-der.ps.gz .
+##
+## Counter-intuitively to some, a small state method is <2X faster than a GIANT
+## space one in hot loop benchmarks.  Less space-less time=win,win.  The costly
+## method is kept as edifying. (Also, CPU/libcs with >2X slower exp/ln exist.)
+
 import std/[math, random, syncio], memfiles as mf
 
 proc calcCDF*(n: int, alpha=1.5): seq[float64] =
@@ -32,11 +41,47 @@ iterator sample*[T](n=10, alpha=1.5, wr="", rd="", keys: openArray[T]): T =
   if not sav.mem.isNil:
     sav.close
 
+# The (usually) a little faster & always much smaller space method.
+
+type Zipf* = object         ## Holds metadata to generate Zipf deviates.
+  s, imax, v, q, one_Q, one_Qinv, hxm, hx0_Hxm: float
+
+func h(z: Zipf, x: float): float = exp(z.one_Q*ln(z.v + x)) * z.one_Qinv
+
+func hI(z: Zipf, h: float): float = exp(z.one_Qinv*ln(z.one_Q*h)) - z.v
+
+func initZipf*(s: float, v: range[1.0..float.high], imax: uint): Zipf =
+  ## Make a Zipf deviate maker which gets values k on [0,imax] such that P(k) ~
+  ## (v + k)**(-s).  Needs: s > 1.
+  if s <= 1.0: raise newException(ValueError, "Zipf needs s > 1.0")
+  result.imax     = imax.float; result.v = v; result.q = s
+  result.one_Q    = 1.0 - s
+  result.one_Qinv = 1.0 / result.one_Q
+  result.hxm      = result.h(result.imax + 0.5)
+  result.hx0_Hxm  = result.h(0.5) - exp(-s*ln(v)) - result.hxm
+  result.s        = 1 - result.hI(result.h(1.5) - exp(-s*ln(v + 1.0)))
+
+func sample*(r: var Rand, z: Zipf): uint =
+  ## Get a value drawn from distribution described by `z` using `r`.
+  if z.v == 0.0: raise newException(ValueError, "Uninitialized Zipf")
+  var k = 0.0
+  while true:
+    let r  = r.rand(1.0)                # U[0,1] sample
+    let ur = z.hxm + r*z.hx0_Hxm
+    let x  = z.hI(ur)
+    k = floor(x + 0.5)                  # round to nearest integer
+    if k - x <= z.s or ur >= z.h(k + 0.5) - exp(-ln(k + z.v)*z.q):
+      break
+  k.uint
+
+proc sample*(z: Zipf): uint = randState.sample(z)
+  ## Get a value drawn from distribution described by `z` with the global PRNG.
+
 when isMainModule:
   import cligen, cligen/osUt
   when defined(release): randomize()
 
-  proc zipf(n=10, alpha=1.5, wr="", rd="", gen=0..0, bin=false,
+  proc zipf(n=10, alpha=1.5, wr="", rd="", gen=0..0, bin=false, fast=false,
             keys: seq[string]) =
     ## Sample passed args according to a Zipf distribution for for n items.
     ## The bigger `alpha` the more skewed to dominant events.  Provides `wr`
@@ -44,10 +89,18 @@ when isMainModule:
     ## (https://github.com/c-blake/nio) filenames ending in `.Nd`.
     if gen.a != 0 or gen.b != 0:
       var keys: seq[int64]
-      for i in gen: keys.add i
+      if not fast:
+        keys.setLen gen.b - gen.a + 1
+        for i in gen: keys[i - gen.a] = i
       if bin:
-        for key in sample(n, alpha, wr, rd, keys):
-          discard stdout.uriteBuffer(key.unsafeAddr, 8)
+        if fast:
+          let z = initZipf(alpha, 1.0, uint(gen.b - gen.a))
+          for _ in 1..n:
+            let key = z.sample
+            discard stdout.uriteBuffer(key.unsafeAddr, 8)
+        else:
+          for key in sample(n, alpha, wr, rd, keys):
+            discard stdout.uriteBuffer(key.unsafeAddr, 8)
       else:
         for key in sample(n, alpha, wr, rd, keys): outu $key, "\n"
     else:
@@ -58,4 +111,5 @@ when isMainModule:
                        "wr"   : "write (binary) data to this",
                        "rd"   : "read (binary) data from this",
                        "gen"  : "sample from ints in this range",
-                       "bin"  : "8B binary ints->stdout"}
+                       "bin"  : "8B binary ints->stdout",
+                       "fast" : "small state method (bin,gen mode only)"}
