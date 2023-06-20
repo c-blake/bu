3e92356282d1bdfad372f36c58c0055f4f85faba
Charles Blake <charlechaud@gmail.com>
2023-06-20 02:53:15 -0400

    Replace `labels:seq[MSlice]` w/a bit mask paired with `labs:seq[MSlice]` in a new `Weights` object type.  Still support dup/multiple counting via additional `cnt` Table.
    This moves some work/complexity from `print` to `loadWeights`.  It also
    limits number of sources to 64 (not limiting for my use cases).  It also
    removes all small allocations (including the old output one for $w).
    
    Operationally, the only difference here (besides the 64 source limit) is
    that the `print` loop is over hash entries, not over tokens looking up
    over & over again.  So, new tokens introduced in SRC files can print,
    and printing is in hash-order which is fine for me.
    
    Since profiling shows so much time is in emission, more minorly trim the
    number of `outu` calls by extending labs to their prior character.

--- wsample.nim
+++ wsample.nim
@@ -4,28 +4,39 @@ proc loadTokens*(tokens: string): seq[MSlice] = # no close to keep result valid
   for token in mopen(tokens).mSlices: result.add(token)
 
 type
-  Weight* = tuple[w: int, labels: seq[MSlice]]
+  Weight* = tuple[w: int32, aMul: bool, why: uint64]
   WeightTab* = Table[MSlice, Weight]
+  Weights* = object
+    wtab: WeightTab
+    labs: seq[MSlice]
+    cnt: Table[MSlice, array[64, uint8]]    # token -> (cnt[labIx] tab for mults
+var emptyArr: array[64, uint8]
 
-proc loadWeights*(weights="", tokens: seq[MSlice]): WeightTab =
-  result = initTable[MSlice, Weight](tokens.len)
-  let empty: Weight = ( 0, newSeqOfCap[MSlice](1) )
-  for line in mopen(weights).mSlices: # no close to keep `label` valid
-    if line.len == 0 or line[0] == '#':
-      continue
-    let cols = line.msplit(0)
-    if cols.len != 3:
-      continue
-    let path  = $cols[0]
-    let amt   = parseInt($cols[1])
-    let label = cols[2]
+proc loadWeights*(weights="", tokens: seq[MSlice]): Weights =
+  result.wtab = initTable[MSlice, Weight](tokens.len)
+  let empty: Weight = (0, false, 0)
+  for line in mopen(weights).mSlices:       # Keep open to keep `label` valid
+    if line.len == 0 or line[0] == '#': continue
+    var cols = line.msplit(0)
+    if cols.len != 3: continue
+    let path  = $cols[0]                    # Format is: PATH WEIGHT LABEL
+    let amt   = parseInt($cols[1]).int32
     if path == "BASE":
-      for token in tokens: result.mgetOrPut(token, empty).w += amt
+      for token in tokens: result.wtab.mgetOrPut(token, empty).w += amt
     else:
-      if (let mf = mopen(path); mf) != nil: # No close in case mgetOrPut
+      if (let mf = mopen(path); mf != nil): # No close in case mgetOrPut
+        if result.labs.len==64: raise newException(IOError, "too many sources")
+        let bit = 1u64 shl result.labs.len
         for token in mf.mSlices:            #..adds a novel `token`.
-          var cell = addr result.mgetOrPut(token, empty)
-          cell.w += amt; cell.labels.add label
+          var cell = addr result.wtab.mgetOrPut(token, empty)
+          cell.w += amt
+          if (cell.why and bit) != 0:       # Bit already on: inc multiples
+            cell.aMul = true                # Avoids lookup-to-test in `print`
+            result.cnt.mgetOrPut(token, emptyArr)[result.labs.len].inc # Updt>1
+          else:                             #XXX saturating `incs` ^^^ overload
+            cell.why = cell.why or bit      # Turn bit on for labs[^1]
+        cols[2].mem = cols[2].mem +! -1; cols[2].len += 1 # extend to delim
+        result.labs.add cols[2]             # @end to avoid earlier -1/^1's
 
 proc weights*(wt: WeightTab, tokens: seq[MSlice]): seq[float] =
   for t in tokens: result.add float(wt[t].w)
@@ -34,23 +45,19 @@ proc meanW(wt: WeightTab): float =
   for wL in wt.values: result += wL.w.float
   result /= wt.len.float
 
-proc print*(wt: WeightTab, tokens: seq[MSlice], stdize=false) =
-  let space = " "; let spc = toMSlice(space)
-  let meanW = if stdize: wt.meanW else: 1.0
-  for tok in tokens:
-    let wL = wt[tok]
-    let ws = if stdize: formatFloat(wL.w.float/meanW, ffDefault, 4) else: $wL.w
-    outu ws, spc, tok
-    var last = spc
-    var cnt = 1
-    for lab in wL.labels:
-      if lab == last: cnt.inc
-      else:
-        if cnt > 1: outu "*", cnt
-        outu spc, lab
-        cnt = 1
-      last = lab
-    if cnt > 1: outu "*", cnt
+proc print*(wts: Weights, stdize=false) =
+  var ws: string
+  let meanW = if stdize: wts.wtab.meanW else: 1.0
+  for tok, val in wts.wtab:             # Need not re-lookup if hash-order&maybe
+    let (w, aMul, why) = val            #..novel keys in SRC notin `tokens` isOk
+    if stdize: ws = formatFloat(w.float/meanW, ffDefault, 4)
+    else: ws.setLen 0; ws.addInt w
+    ws.add " "; ws.add tok; outu ws     # 1 outu faster than outu ws, " ", tok
+    let cnt = if aMul: wts.cnt[tok] else: emptyArr # Good C compiler skips cpy..
+    for i, lab in wts.labs:                        #..unless cnt[i] needed below
+      if (why and (1u64 shl i)) != 0:
+        outu lab
+        if aMul and cnt[i] > 0: outu "*", $(cnt[i] + 1) # `cnt` counts > 1
     outu "\n"
 
 iterator cappedSample*(wt: WeightTab, tokens: seq[MSlice], n=1, m=3): MSlice =
@@ -78,12 +85,12 @@ when isMainModule:
     ## nl-delimited tokens.  BASE in `weights` = `tokens` (gets no label).
     setCurrentDir(dir)
     let tokens = loadTokens(tokens)
-    let wt = loadWeights(weights, tokens)
-    if explain: wt.print(tokens, stdize); quit 0
+    let wts = loadWeights(weights, tokens)
+    if explain: wts.print stdize; quit 0
     if m > 0:
-      for s in wt.cappedSample(tokens, n, m): echo s
+      for s in wts.wtab.cappedSample(tokens, n, m): echo s
     else:
-      let cdf = wt.weights(tokens).cumsummed
+      let cdf = wts.wtab.weights(tokens).cumsummed
       for i in 1..n: echo sample(tokens, cdf)
   dispatch wsample,help={"weights": "path to weight meta file",
                          "tokens" : "path to tokens file",
