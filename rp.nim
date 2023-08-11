9c190de67361a452339e2e4ede163c22d16975c4
Charles Blake <charlechaud@gmail.com>
2023-08-11 16:25:28 -0400

    Make e.g. order match `crp.nim` & end with very Nim stdlib-specific one.

--- rp.nim
+++ rp.nim
@@ -36,9 +36,9 @@ proc rp(prelude="", begin="", where="true", stmts:seq[string], epilog="",
   ##   **rp 'echo s[1]," ",s[0]'**                     # Swap field order
   ##   **rp -b'var t=0' t+=nf -e'echo t'**             # Print total field count
   ##   **rp -b'var t=0' -w'0.i>0' t+=0.i -e'echo t'**  # Total >0 field0 ints
-  ##   **rp -p'import stats' -b'var r: RunningStat' 'r.push 0.f' -e'echo r'**
   ##   **rp 'let x=0.f' 'echo (1+x)/x'**               # cache field 0 parse
   ##   **rp -d, -fa,b,c 'echo s[a],b.f+c.i.float'**    # named fields (CSV)
+  ##   **rp -p'import stats' -b'var r:RunningStat' 'r.push 0.f' -e'echo r'**
   ## Add niceties (eg. `import lenientops`) to *prelude* in ~/.config/rp.
   let prelude = if stmts.len > 0: prelude
                 else: "when not declared(stdout): import std/syncio\n" & prelude
