0e3093a732ade812bf08fc6f8763aba0fbbff4d0
Charles Blake <charlechaud@gmail.com>
2023-09-10 07:54:17 -0400

    Explain why the `type` def & re-align help table.

--- cols.nim
+++ cols.nim
@@ -1,9 +1,9 @@
 when not declared(fmWrite): import std/syncio
 import std/sets, cligen, cligen/[mfile, mslice, osUt] # mSlices MSlice Sep
 
-type Range = Slice[int]
+type Ranges = seq[Slice[int]] # cg wants `seq[T]` syntax *OR* its semantics
 proc cols(input="/dev/stdin", delim="white", output="/dev/stdout", sepOut=" ",
-          blanksOk=false, cut=false, origin=1, term='\n',colRanges: seq[Range])=
+          blanksOk=false, cut=false, origin=1, term='\n', colRanges: Ranges) =
   ## Write just some columns of input to output; Memory map input if possible.
   var outFile = open(output, fmWrite)
   var colSet = initHashSet[int](colRanges.len)
@@ -35,11 +35,11 @@ proc cols(input="/dev/stdin", delim="white", output="/dev/stdout", sepOut=" ",
 
 when isMainModule: dispatch cols, help={
   "colRanges": "colNums or A..B | X:Y (in|ex)clusive ranges thereof",
-  "input"   : "path to mmap|read as input",
-  "delim"   : "inp delim chars; Any repeats => fold",
-  "output"  : "path to write output file",
-  "sepOut"  : "output field separator",
-  "blanksOk": "allow blank output rows",
-  "cut"     : "cut/censor specified columns, not keep",
-  "origin"  : "origin for colNums; 0=>signed indexing",
-  "term"    : "set row terminator (e.g. \\\\0)"}
+  "input"    : "path to mmap|read as input",
+  "delim"    : "inp delim chars; Any repeats => fold",
+  "output"   : "path to write output file",
+  "sepOut"   : "output field separator",
+  "blanksOk" : "allow blank output rows",
+  "cut"      : "cut/censor specified columns, not keep",
+  "origin"   : "origin for colNums; 0=>signed indexing",
+  "term"     : "set row terminator (e.g. \\\\0)"}
