3b4f871bd039339f83db3632e5a621d7c58cf3f3
Charles Blake <charlechaud@gmail.com>
2023-09-11 14:21:37 -0400

    Add a feature to allow any input row delimiter, not just newline.

--- cols.nim
+++ cols.nim
@@ -2,8 +2,9 @@ when not declared(fmWrite): import std/syncio
 import std/sets, cligen, cligen/[mfile, mslice, osUt] # mSlices MSlice Sep
 
 type Ranges = seq[Slice[int]] # cg wants `seq[T]` syntax *OR* its semantics
-proc cols(input="/dev/stdin", delim="white", output="/dev/stdout", sepOut=" ",
-  blanksOk=false, cut=false, origin=1, O0=false, term='\n', colRanges: Ranges) =
+proc cols(input="/dev/stdin", rowDlm='\n', delim="white", output="/dev/stdout",
+          sepOut=" ", blanksOk=false, cut=false, origin=1, O0=false, term='\n',
+          colRanges: Ranges) =
   ## Write just some columns of input to output; Memory map input if possible.
   let origin = if O0: 0 else: origin
   var outFile = open(output, fmWrite)
@@ -13,7 +14,7 @@ proc cols(input="/dev/stdin", delim="white", output="/dev/stdout", sepOut=" ",
       for c in r: colSet.incl c
   let sep = initSep delim
   var cols: seq[MSlice] = @[ ]
-  for line in mSlices(input, eat='\0'): # RO mmap | slices from stdio
+  for line in mSlices(input, sep=rowDlm, eat='\0'): # RO mmap | 1-use slices
     var wrote = false                   # wrote something &so need sepOut|\n
     sep.split line, cols
     if cut:
@@ -37,11 +38,12 @@ proc cols(input="/dev/stdin", delim="white", output="/dev/stdout", sepOut=" ",
 when isMainModule: dispatch cols, short={"O0": '0'}, help={
   "colRanges": "colNums or A..B | X:Y (in|ex)clusive ranges thereof",
   "input"    : "path to mmap|read as input",
-  "delim"    : "inp delim chars; Any repeats => fold",
+  "rowDlm"   : "inp *row* delimiter character",
+  "delim"    : "inp *field* dlm chars; len>0 => fold",
   "output"   : "path to write output file",
   "sepOut"   : "output field separator",
   "blanksOk" : "allow blank output rows",
   "cut"      : "cut/censor specified columns, not keep",
   "origin"   : "origin for colNums; 0=>signed indexing",
   "O0"       : "shorthand for `--origin=0`",
-  "term"     : "set row terminator (e.g. \\\\0)"}
+  "term"     : "set output row terminator (e.g. \\\\0)"}
