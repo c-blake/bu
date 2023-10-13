4cf5b60cc41781776d6db62bc5748d6a634fef01
Charles Blake <charlechaud@gmail.com>
2023-10-13 05:40:44 -0400

    Only ~2% faster to pack into 8B not 16B, but a very consistent 2%.

--- wsample.nim
+++ wsample.nim
@@ -5,17 +5,20 @@ proc loadTokens*(tokens: string): seq[MSlice] = # no close to keep result valid
   for token in mopen(tokens).mSlices: result.add(token)
 
 type
-  Weight* = tuple[w: int32, aMul: bool, why: uint64]
+  Weight* {.packed.} = object       ## Size=8B
+    w*    {.bitsize: 15.}: int16    ## weight: up to 32767
+    aMul* {.bitsize:  8.}: bool     ## lookup-avoidance-flag: >1 multiplicity
+    why*  {.bitsize: 48.}: uint64   ## explanation mask
   WeightTab* = Table[MSlice, Weight]
   Weights* = object
     wtab: WeightTab
     labs: seq[MSlice]
-    cnt: Table[MSlice, array[64, uint8]]    # token -> (cnt[labIx] tab for mults
-var emptyA: array[64, uint8]
+    cnt: Table[MSlice, array[48, uint8]]    # token -> (cnt[labIx] tab for mults
+var emptyA: array[48, uint8]
 
 proc loadWeights*(weights="", tokens: seq[MSlice]): Weights =
   result.wtab = initTable[MSlice, Weight](tokens.len)
-  let empty: Weight = (0, false, 0)
+  const empty = Weight()
   for line in mopen(weights).mSlices:       # Keep open to keep `label` valid
     var line = line
     if line.len > 0: line.clipAtFirst '#'
@@ -23,12 +26,12 @@ proc loadWeights*(weights="", tokens: seq[MSlice]): Weights =
     var cols = line.msplit(0)
     if cols.len != 3: continue
     let path = $cols[0]                     # Format is: PATH WEIGHT LABEL
-    let amt  = parseInt($cols[1]).int32
+    let amt  = parseInt($cols[1]).int16
     if path == "BASE":
       for token in tokens: result.wtab.mgetOrPut(token, empty).w += amt
     else:
       if (let mf = mopen(path); mf != nil): # No close in case mgetOrPut
-        if result.labs.len==64: raise newException(IOError, "too many sources")
+        if result.labs.len==48: raise newException(IOError, "too many sources")
         let bit = 1u64 shl result.labs.len
         for token in mf.mSlices:            #..adds a novel `token`.
           if token.len == 0: continue
@@ -52,16 +55,15 @@ proc meanW(wt: WeightTab): float =
 proc print*(wts: Weights, stdize=false) =
   var ws: string
   let meanW = if stdize: wts.wtab.meanW else: 1.0
-  for tok, val in wts.wtab:             # Need not re-lookup if hash-order&maybe
-    let (w, aMul, why) = val            #..novel keys in SRC notin `tokens` isOk
-    if stdize: ws = formatFloat(w.float/meanW, ffDefault, 4)
-    else: ws.setLen 0; ws.addInt w
+  for tok, v in wts.wtab:   # hash-order; Maybe novel keys in SRC notin `tokens`
+    if stdize: ws = formatFloat(v.w.float/meanW, ffDefault, 4)
+    else: ws.setLen 0; ws.addInt v.w
     ws.add " "; ws.add tok; outu ws     # 1 outu faster than outu ws, " ", tok
-    let cnt = if aMul: wts.cnt[tok] else: emptyA # Good C compiler skips copy..
-    for i, lab in wts.labs:                      #..unless cnt[i] needed below.
-      if (why and (1u64 shl i)) != 0:
+    let cnt = if v.aMul: wts.cnt[tok] else: emptyA # Good C compiler skips cpy..
+    for i, lab in wts.labs:                        #..unless cnt[i] needed below
+      if (v.why and (1u64 shl i)) != 0:
         outu lab
-        if aMul and cnt[i] > 0: outu "*", $(cnt[i] + 1) # `cnt` counts > 1
+        if v.aMul and cnt[i] > 0: outu "*", $(cnt[i] + 1) # `cnt` counts > 1
     outu "\n"
 
 iterator cappedSample*(wt: WeightTab, tokens: seq[MSlice], n=1, m=3): MSlice =
