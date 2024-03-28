9c8300af29d347013096cdcb8df4983a8ea17a4e
Charles Blake <charlechaud@gmail.com>
2024-03-28 16:24:11 -0400

    "Vectorize" `dirq` with simply repeated `-da pf1 pf2 -db pf3 pf4` and then a `select` loop.  Binding the -d to its following positionals needed bypassing much `cligen` automation, but the result is (mostly) backward compatible at the CLI level and I kind of like the result.
    The singular iterator becomes deprecated.  Also, placate Nim warning
    system with dir.cstring.
    
    As part of the help update, just list all event kind names.

--- dirq.nim
+++ dirq.nim
@@ -3,6 +3,7 @@ when not defined(linux):
 FreeBSD kqueue are possible, likely with translation of abstractions."""
 else:
  import posix/inotify, std/posix, cligen, cligen/[sysUt, posixUt]
+ when not declared(stderr): import std/syncio
 
  type Event* = enum
   inAccess    ="access"   , inAttrib ="attrib"    , inModify    ="modify"      ,
@@ -28,37 +29,95 @@ else:
     of inDeleteSelf: result |= IN_DELETE_SELF
 
  iterator dqueue*(dir: string; events={inMovedTo, inCloseWr}):
-    tuple[name: ptr char; len: int] =
+    tuple[name: ptr char; len: int] {.deprecated: "use `dqueues`".} =
   ## Set up event watches on dir & forever yield NUL-terminated (ptr char,len).
   if chdir(dir) == -1:
     raise newException(OSError, "chdir(\"" & dir & "\")")
   let fd = cint(inotify_init())
   if fd == -1:
     raise newException(OSError, "inotify_init")
-  if inotify_add_watch(fd, dir, events.mask or IN_ONLYDIR) == -1:
+  if inotify_add_watch(fd, dir.cstring, events.mask or IN_ONLYDIR) == -1:
     raise newException(OSError, "inotify_add_watch")
   var evs = newSeq[byte](8192)
   while (let n = read(fd, evs[0].addr, 8192); n) > 0:
     for ev in inotify_events(evs[0].addr, n):
       yield (ev[].name.addr, int(ev[].len))
 
- when isMainModule:
-  proc dirq(events={inMovedTo, inCloseWr}; dir="."; wait=false;
-            cmdPrefix: seq[string]): int =
-    ## chdir(*dir*) & wait for events to occur on it.  For each delivered event,
-    ## run *cmdPrefix* **NAME** where **NAME** is the filename (not full path)
-    ## delivered.  Default events are any writable fd-close on files in *dir* or
-    ## rename into *dir* (usually signaling **NAME** is ready as an input file).
-    let n   = cmdPrefix.len                     # index of a new last slot
-    let cmd = allocCStringArray(cmdPrefix & "") # setup ready-to-exec buffer
-    if not wait:
-      signal(SIGCHLD, reapAnyKids)              # Block zombies
-    for (name, nmLen) in dqueue(dir, events):
-      if nmLen > 0 or name != nil:
-        cmd[n] = cast[cstring](name)            # Poke ptr char into slot and..
-        discard cmd.system(wait)                #..run command, maybe in bkgd
+ iterator dqueues*(events: set[Event]; dirs: seq[string]):
+    tuple[i, len: int; name: ptr char] =
+  ## Set up event watches on dirs & forever yield NUL-term (i, len, ptr char).
+  var rd0: TFdSet; var fdMax = 0.cint
+  FD_ZERO rd0
+  var fds = newSeq[cint](dirs.len)
+  for i, dir in dirs:
+    fds[i] = cint(inotify_init())
+    if fds[i] == -1: raise newException(OSError, "inotify_init")
+    if inotify_add_watch(fds[i], dir.cstring, events.mask or IN_ONLYDIR) == -1:
+      raise newException(OSError, "inotify_add_watch")
+    FD_SET fds[i], rd0
+    fdMax = max(fdMax, fds[i])
+    discard fcntl(fds[i], F_SETFL, O_NONBLOCK)  # Set to non-blocking
+  var evs = newSeq[byte](8192)
+  var rd = rd0
+  while select(fdMax + 1, rd.addr, nil, nil, nil) != -1 or errno == EINTR:
+    for i, fd in fds:
+      if FD_ISSET(fd, rd) != 0:
+        while (let n = read(fd, evs[0].addr, 8192); n) > 0 or errno == EINTR:
+          for ev in inotify_events(evs[0].addr, n):
+            yield (i, int(ev[].len), ev[].name.addr)
+        if errno notin [EAGAIN, EWOULDBLOCK]:
+          stderr.write "dirq errno: ",errno,"\n"
+    rd = rd0
 
-  dispatch(dirq, help = { "events": "inotify event types to use; " &
-                                    "Giving \"x\" lists all",
+ when isMainModule:
+  var parse: seq[ClParse]
+  proc dirq(events={inMovedTo, inCloseWr}; wait=false; dir=".";
+            cmdPrefix: seq[string]) = discard
+  dispatchGen dirq, help={"events": "inotify event types to use",
                           "dir"   : "directory to watch",
-                          "wait"  : "wait4(kid) until re-launch" })
+                          "wait"  : "wait4(kid) until re-launch"},
+              setByParse=addr parse, doc="""
+chdir(*dir*) & wait for events to occur on it.  For each delivered event, run
+*cmdPrefix* **NAME** where **NAME** is the filename (*NOT* full path) delivered.
+
+Handleable events are:
+  `access`    `attrib`  `modify`   `open`   `closeWrite` `closeNoWrite`
+  `movedFrom` `movedTo` `moveSelf` `create` `delete`     `deleteSelf`
+
+Default events `closeWrite` (any writable fd-close) | `movedTo` (renamed into
+*dir*) usually signal that **NAME** is ready as an input file.
+
+**dirq** can watch & dispatch for many dirs at once with repeated *--dir=A
+cmdPfx for A --dir=B cmdPfx for A* patterns; *events* & *wait* are global."""
+  dispatchDirq parseOnly=true
+  if clHelpOnly in parse:
+    stdout.write parse[parse.next({clHelpOnly})].message; quit 0
+  for pe in parse:
+    if pe.status notin [clOk, clPositional]: stderr.write pe.message,'\n';quit 1
+  let eventsD = {inMovedTo, inCloseWr}; var events: set[Event]
+  let waitD = false; var wait = waitD
+  var dirs: seq[string]; var args: seq[seq[string]] # Folders & Initial args
+  var a: ArgcvtParams
+  for pe in parse:                              # Parses must all succeed since
+    case pe.paramName                           #..all got clOk|Pos in 1st pass.
+    of "wait"  : discard argParse(wait, waitD, a)
+    of "events": # Need parseopt3 for fancy `sep`; Instead start {}, only add &
+      a.key = "events"; a.sep = "="; a.val = pe.unparsedVal #..fallback to Defl.
+      discard argParse(events, eventsD, a); echo "new events: ", events
+    of "dir"   : dirs.add pe.unparsedVal
+    else: # Above clOk check ensures clPositional here; Accumulate onto args[].
+      if dirs.len == 0: dirs.add "."
+      if args.len != dirs.len: args.add @[]
+      args[^1].add pe.unparsedVal
+  if events.card==0: events = eventsD           # End of verbose~manual CL-parse
+  if not wait: signal(SIGCHLD, reapAnyKids)     # Block zombies
+  var cmds = newSeq[cstringArray](dirs.len)     # Setup ready-to-exec buffer
+  var ns   = newSeq[int](dirs.len)              # Indices of new last slots
+  for arg in args.mitems: arg.add ""            # Pre-prepare CStringArray's
+  for i, arg in args: ns[i] = arg.len - 1; cmds[i] = arg.allocCStringArray
+  for (i, nmLen, name) in dqueues(events, dirs):
+    if nmLen > 0 or name != nil:
+      if chdir(dirs[i].cstring) == -1:
+        raise newException(OSError, "chdir \"" & dirs[i] & "\"")
+      cmds[i][ns[i]] = cast[cstring](name)      # Poke ptr char into slot and..
+      discard cmds[i].system(wait)              #..run command, maybe in bkgd
