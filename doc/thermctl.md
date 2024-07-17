4248fafb2143382744993933c4ce17a85ff6a91f
Charles Blake <charlechaud@gmail.com>
2024-07-17 07:50:32 -0400

    Recent hot ambients lead to a couple improvements here: never CONT pids the controller did not also stop and allow `--incl` of processes that are not marked Runnable.  Also update documentation.

--- doc/thermctl.md
+++ doc/thermctl.md
@@ -1,5 +1,4 @@
-Motivation
-----------
+# Motivation
 
 OS kernels can down clock CPUs, but are often not aggressive enough to block
 thermal shutdown.  This controller can sometimes do better.
@@ -8,30 +7,49 @@ This approach is limited, but still useful for me (e.g. on old laptops with
 failing fans &| overclocked gamer rigs that only overheat with just the right L3
 cache loads, often from g++ compiles).
 
-Operation
----------
+# Operation
 
 At CPU temperature `T > temp.b`, `thermctl` sends SIGSTOP to all runnable PIDs
 & at `T <= temp.a`, it sends SIGCONT to all stopped PIDs.
 
-Limitations
------------
+# Limitations
 
 Pausing can fail to block future work (loadAvg-targeting work ctl, permissions,
 rarely scheduled dispatchers, ..).  Operation can also undesirably SIGCONT jobs
 stopped in shells with job control.
 
-Temperature query & Parameter tuning
-------------------------------------
+# Temperature query & Parameter tuning
+
 ```
-thermctl [optional-params]
+  thermctl [optional-params] 
+OS kernels can down clock CPUs but may not be aggressive enough to block thermal
+shutdown. This controller can sometimes do better. At T > temp.b, it SIGSTOPs
+runnable PIDs & at T <= temp.a, it SIGCONTs PIDs it stopped.
+
+NOTE: Pausing can fail to block future work (loadAvg-targeting dispatch, perms,
+hot procs often put to sleep just before scheduling thermctl itself, etc.).
+So, this approach is limited, but maybe useful (e.g. on old laptops with failing
+fans &| overclocked gamer rigs).
+Options:
   -q=, --qry=   string  "auto"     auto:Intel?turbostat -sCPU,CoreTmp:cpuTemp
-  -i=, --ival=  float   1.0        $1 param to qry (likely a delay)
+  -d=, --delay= float   1.0        $1 param to qry (likely a delay)
   -m=, --match= string  "."        pattern selecting cpuTemp line
-  -e=, --excl=  strings thermctl   cmd names to never SIGSTOP
-  -l=, --log=   string  ""         path to log control transitions to
   -t=, --temp=  Slice   80.0..90.0 > b => pause; < a => resume
+  -l=, --log=   string  ""         path to log control transitions to
+  -i=, --incl=  strings ffmpeg     cmd names to always SIGSTOP if hot
+  -e=, --excl=  strings thermctl   cmd names to never SIGSTOP
 ```
+
+# A Few More Details
+
+The idea of `--incl` is to avoid a noticed problem where main heat generating
+processes are suspended very briefly just before switching to turbostat/thermctl
+itself.  That means they are not marked Runnable (& so not sent SIGSTOP) for
+maybe several query cycles in a row.  I saw as many as 4..6 sequentially on a
+4-core.  That allows temperature to rise too quickly.  A good rule of thumb for
+this parameter might be processes that tend to be long-lived and CPU-heavy that
+you are sure it's ok to SIGSTOP & SIGCONT like ffmpeg or gcc.
+
 Note that `turbostat` is distributed with Linux kernel sources.  So, if you
 build your own kernels you can usually get it with
 ```
--- thermctl.nim
+++ thermctl.nim
@@ -1,78 +1,85 @@
-import std/[os, osproc, posix, strformat, strutils, re, times], cligen/osUt
+import std/[os,osproc,posix, strformat,strutils,re, times, sets], cligen/osUt
 when not declared(File): import std/[syncio, formatfloat]
 
-proc log(o: File, t0: var DateTime, msg: string) {.raises: [].} =
+proc `$`(x: HashSet[Pid]): string = 
+  for pid in x: (if result.len > 0: result.add ' '); result.add $pid
+
+proc log(o: File, t0: var DateTime, msg: string) =
  try:
   let t1 = now(); let diff = t1 - t0; t0 = t1
   o.write t1.format("yyyy/MM/dd-HH:mm:ss,ffffff")#'.' notin times.FormatLiterals
-  o.write ": ", msg, " after ", diff.inSeconds, " seconds\n"; o.flushFile
+  o.write ": ", msg, " after ", diff.inMilliseconds, " ms\n"; o.flushFile
  except Ce: discard
 
-iterator pidStates(excl: seq[string]): (Pid, string) = # yield non-excl (pid,st)
+proc pidStCmd(pid: string): (Pid, char, string) =
+  let ps = readFile("/proc/" & pid & "/stat") # Parse PID (CMD) STATE...
+  let eoPid = ps.find(" (")      # Bracket CMD. Works even if CMD has
+  let eoCmd = ps.rfind(") ")     #..parens or whitespace chars in it.
+  if eoPid != -1 and eoCmd != -1 and pid == ps[0 ..< eoPid]:
+    result[0] = parseInt(ps[0 ..< eoPid]).Pid
+    if (let eoSt = ps.find(' ', start=eoCmd + 2); eoSt != -1):
+      result[1] = ps[eoCmd + 2 ..< eoSt][0]
+    result[2] = ps[eoPid + 2 ..< eoCmd]
+
+iterator pidStates(): (Pid, char, string) = # yield (pid, st, cmd)
   try:      # Process list is dynamic => time-of-walk/time-of-parse|act errors
     for pcKind, pid in walkDir("/proc", relative=true):
-      if pcKind == pcDir and pid.len > 0 and pid[0] in Digits:
-        let statB = readFile("/proc/" & pid & "/stat") #Parse PID (CMD) STATE...
-        let eoPid = statB.find(" (")      # Bracket CMD. Works even if CMD has
-        let eoCmd = statB.rfind(") ")     #..parens or whitespace chars in it.
-        if eoPid != -1 and eoCmd != -1 and pid == statB[0 ..< eoPid]:
-          let pid = parseInt(statB[0 ..< eoPid]).Pid
-          let cmd = statB[eoPid + 2 ..< eoCmd]
-          if cmd notin excl:
-            let eoState = statB.find(' ', start=eoCmd+2)
-            if eoState != -1:
-              yield (pid, statB[eoCmd+2 ..< eoState])
+      if pcKind == pcDir and pid.len>0 and pid[0] in Digits: yield pidStCmd(pid)
   except Ce: discard
 
-proc stop(excl: seq[string]) {.raises: [].} =
-  for (pid, state) in pidStates(excl):
-    if state == "R": discard kill(pid, SIGSTOP)
+var pids: HashSet[Pid]
 
-proc cont(excl: seq[string]) {.raises: [].} =
-  for (pid, state) in pidStates(excl):
-    if state == "T": discard kill(pid, SIGCONT)
+proc stop(o: File; t0: var DateTime; T: float; incl, excl: seq[string]) =
+  for (pid, state, cmd) in pidStates():
+    if cmd notin excl and (cmd in incl or state == 'R'):
+      discard kill(pid, SIGSTOP); pids.incl pid
+  o.log t0, &"T {T} C; Paused [{$pids}]"
 
-proc thermctl*(qry="auto", ival=1.0, match=".", excl = @["thermctl"], log="",
-               temp=80.0..90.0) =
-  ## OS kernels can down clock CPUs but are often not aggressive enough to block
+proc cont(o: File; t0: var DateTime; T: float) =
+  for pid in pids:
+    let (_, state, _) = pidStCmd($pid)
+    if state == 'T': discard kill(pid, SIGCONT)
+  o.log t0, &"T {T} C; Resume [{$pids}]"
+  pids.clear
+
+proc thermctl*(qry="auto", delay=1.0, match=".", temp=80.0..90.0, log="",
+               incl = @["ffmpeg"], excl = @["thermctl"]) =
+  ## OS kernels can down clock CPUs but may not be aggressive enough to block
   ## thermal shutdown. This controller can sometimes do better. At `T > temp.b`,
-  ## it SIGSTOPs runnable PIDs & at `T <= temp.a`, it SIGCONTs stopped PIDs.
+  ## it SIGSTOPs runnable PIDs & at `T <= temp.a`, it SIGCONTs PIDs it stopped.
   ##
-  ## NOTE: Pausing can fail to block future work (loadAvg-targeting work ctl,
-  ## permissions, rarely scheduled dispatchers, ..).  This can also continue
-  ## shell job ctl stopped jobs.  So, this approach is limited, but still useful
-  ## for me (e.g. on old laptops with failing fans &| overclocked gamer rigs).
+  ## NOTE: Pausing can fail to block future work (loadAvg-targeting dispatch,
+  ## perms, hot procs often put to sleep just before scheduling thermctl itself,
+  ## etc.).  So, this approach is limited, but maybe useful (e.g. on old laptops
+  ## with failing fans &| overclocked gamer rigs).
   var excl  = excl
   var qry   = qry                           # Default&massage qry&match params
   var match = match
   if qry == "auto":
-    if "Intel" in (try: execCmdEx("uname -p")[0] except Ce: ""): # Nim for $()
-      qry = "turbostat -s CPU,CoreTmp -qi$1"; match = "^-"; excl.add "turbostat"
-    else:
-      qry = "cpuTemp $1"; match = "."; excl.add "cpuTemp"; excl.add "sensors"
+   if "Intel" in (try: execCmdEx("uname -p")[0] except Ce: ""): # Nim for $()
+    qry="exec turbostat -s CPU,CoreTmp -q -i $1";match="^-";excl.add "turbostat"
+   else:
+    qry="exec cpuTemp $1"; match = "."; excl.add "cpuTemp"; excl.add "sensors"
   let rx      = re(match)
   let o       = if log.len > 0: open(log, fmAppend) else: stdout
   var t0      = now()                       # Want to log time of stop & cont
   var cooling = false
-  for line in popenr(qry % $ival).lines:    # WANT to die if raises IOError here
+  for line in popenr(qry % $delay).lines:   # WANT to die if raises IOError here
     if line.contains(rx):                   # Only whole CPU records not /Core
       var cpuTemp = 0.0
       try      : cpuTemp = parseFloat(line.split()[^1])
-      except Ce: stderr.write "could not parse cpuTemp in: ",line,"\n"; continue
+      except Ce: o.log t0, &"no cpuTemp in: {line}"; continue
       if cooling and cpuTemp <= temp.a:
-        o.log t0, &"cpuTemp {cpuTemp} C; Resuming"
-        cont excl
-        cooling = false
+        o.cont t0, cpuTemp; cooling = false
       elif cpuTemp > temp.b:
-        o.log t0, &"cpuTemp {cpuTemp} C; Pausing"
-        stop excl
-        cooling = true
+        o.stop t0, cpuTemp, incl, excl; cooling = true
 
 when isMainModule:
   import cligen; include cligen/mergeCfgEnv; dispatch thermctl, help={
     "qry"  : "auto:Intel?turbostat -sCPU,CoreTmp:cpuTemp",
-    "ival" : "$1 param to `qry` (likely a delay)",
+    "delay": "$1 param to `qry` (likely a delay)",
     "match": "pattern selecting cpuTemp line",
-    "excl" : "cmd names to never SIGSTOP",
+    "temp" : "`> b` => pause; `< a` => resume",
     "log"  : "path to log control transitions to",
-    "temp" : "`> b` => pause; `< a` => resume"}
+    "incl" : "cmd names to always SIGSTOP *if hot*",
+    "excl" : "cmd names to never SIGSTOP"}
