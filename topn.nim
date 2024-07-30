1600cd528bf025922012665b8e34688a8e46c139
Charles Blake <charlechaud@gmail.com>
2024-07-30 05:15:00 -0400

    Simplify, generalize&speed-up w/new `adix/topk` module.  Specifically:
     - Can just push & later iterate as per `adix/topk`
    
     - Allow order specification of emitted files, including the 2 new
       orders Descending & Cheap
    
     - Allow tuning quick-partition strategy (e.g. to set to `ran` to be
       defensive)
    
     - Add config file / environment processing (to maybe soft-code `-pr`
       for the paranoid).

--- topn.nim
+++ topn.nim
@@ -1,10 +1,11 @@
-import cligen, cligen/[mfile, mslice, osUt], std/heapqueue
+import cligen, cligen/[mfile, mslice, osUt], adix/topk
 from std/strutils as su import nil
 when not declared(stderr): import std/syncio
 
 proc pyIx[T](vs: openArray[T], i: int): T = vs[if i < 0: i + vs.len else: i]
 
-proc topn*(input="/dev/stdin", delim=" ", mxCol=0, n=0, specs: seq[string]) =
+proc topn*(input="/dev/stdin", delim=" ", mxCol=0, n=0, order=Cheap,
+           partn=Partn.last, specs: seq[string]) =
   ## Write spec'd cols of topN-rows-by-various-other-cols to outFile's.  A spec
   ## is `<n>[,<sort-key-col>(0)[,outCol(same)[,outFile(stdout)]]]`.  ColNos are
   ## Py-like 0-origin,signed.  Algo is fast one-pass over (mmap|stream) input.
@@ -34,28 +35,26 @@ proc topn*(input="/dev/stdin", delim=" ", mxCol=0, n=0, specs: seq[string]) =
 
   template sweep(mf, T, i, outsVal) {.dirty.} =
     type Rec = tuple[val: float32; outs: T]
-    var hqs = newSeq[HeapQueue[Rec]](m)
+    var tops: seq[TopK[Rec]]
+    for i in 0 ..< m: tops.add initTopK[Rec](nTop[i], partn)
     var rec: Rec
     for line in mSlices(mf, eat='\0'):  # RO mmap | slices from stdio
       sep.split(line, row, mxCol)       # split into columns
-      for i in 0 ..< m:                 # update our 1-to-several HeapQueues
-        rec.val  = parseFloat(pyIx(row, keyC[i])).float32
-        rec.outs = outsVal          #XXX Better to make `int` index to saved?
-        if hqs[i].len < nTop[i]:    # memcmp savings dep.on float key uniqueness
-          hqs[i].push rec.move      # `$pyIx(..)` in outsVal made any needed cpy
-        elif rec > hqs[i][0]:
-          discard hqs[i].replace rec.move
-    for i in 0 ..< m:                   # Pop out like sort -gk<C+1>|tail -n<n>
-      while hqs[i].len > 0:
-        oFil[i].urite hqs[i].pop.outs, "\n"
-      if not oFil[i].isNil and oFil[i] != stdout:
-        oFil[i].close
+      for i in 0 ..< m:                 # update our 1-to-several `TopK`
+        rec.val  = parseFloat(pyIx(row, keyC[i])).float32 # tuned4 float rarity
+        rec.outs = outsVal
+        tops[i].push rec.move           # `$pyIx(..)` in outsVal maybe made cpy
+    for i in 0 ..< m:                   # Emit like sort -gk<C+1>|tail -n<n>|tac
+      for e in tops[i].maybeOrdered(order): oFil[i].urite e.outs, "\n"
+      if not oFil[i].isNil and oFil[i] != stdout: oFil[i].close
 
   if mf.mem.isNil: sweep(input, string, i, $pyIx(row, oCol[i]))
-  else           : sweep(mf   , MSlice, i, pyIx(row, oCol[i]))
+  else           : sweep(mf   , MSlice, i,  pyIx(row, oCol[i]))
 
-when isMainModule: dispatch topn, help={
+when isMainModule: include cligen/mergeCfgEnv; dispatch topn, help={
   "input": "input data path",
   "delim": "delimiting (repeats=>any num; \"white\")",
   "mxCol": "max columns in input to parse",
-  "n"    : "scale for '%' amounts"}
+  "n"    : "scale for '%' amounts",
+  "partn": "partition: last, ran",
+  "order": "order: Cheap, Ascending, Descending"}
