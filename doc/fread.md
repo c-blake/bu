865ff2f0e41f21b1bcf488c478fc01bad2e3997d
Charles Blake <charlechaud@gmail.com>
2024-08-26 07:58:54 -0400

    Embrace the fact that for a long time `cligen/mfile` has had Windows support to make this little utility apply more broadly.  Update the docs with new usage and a fully worked out example using `fitl`.

--- doc/fread.md
+++ doc/fread.md
@@ -21,12 +21,57 @@ evict other data (more portably than /proc/sys/vm/drop_caches) for cold-cache
 runs, measure drive/pipe or device throughput, etc.  Eg. in Zsh you can say:
 `fread \*\*` or `fread -l $((1<<30)) < /dev/urandom`.
 
-Users may pass paths to FIFOs/named pipes or other block-on-open special files.
-We want to skip those but also avoid check-then-use races.  So, open O_NONBLOCK,
-but then immediately fstat to skip non-regular & reactivate blocking with fcntl
-to be CPU-sharing-friendly.
+Users may pass paths to FIFOs/named pipes/other block-on-open special files
+which are skipped.  Anything named is only used if mmap-able & only 1 byte
+(really 1 cache line) per 4096 is used by the process.  Can use multiple passes
+to measure DIMM bandwidth through a CPU prefetching lens.
 
 Options:
-  -b=, --bsz=    int     16384  buffer size for IO
-  -l=, --limit=  uint64  0      max bytes to read; 0=>unlimited
+  -b=, --bsz=   int   65536 buffer size for stdin IO
+  -l=, --lim=   int64 0     max bytes to read; 0=>unlimited
+  -n=, --nPass= int   1     passes per file
+  -o=, --off=   int   64    total [off0-origin-pass within pages]
+  -v, --verb    bool  false print bytes read
 ```
+
+Example:
+--------
+As just one example benchmark-y kind of sketch, we can easily create a 1 GiB
+file from `/dev/urandom`[^1], and then (w/`taskset` & `chrt`[^2] to lessen
+noise) loop over a series of numbers of passes and fit run times to a [linear
+model](https://github.com/c-blake/fitl):
+
+```sh
+export j=/dev/shm/junk
+dd if=/dev/random of=$j bs=32k count=32k
+taskset -c 2 chrt 99 sh -c \
+ 'for n in `seq 1 64`;do printf "$n ";fread -vn$n $j;done'|
+  fitl -b99 -c,=,n,b 6 0 1
+```
+That yields for me, on one rather old bare metal machine:
+```
+$6= 0.070379 + 1.32172e-03 *$1
+
+bootstrap-stderr-corr matrix
+ 0.0001110    -0.8542
+            3.245e-06
+```
+The very small errors on slope & intercept suggest a good fit & the fit suggests
+initial pass time of 70.38 +- 0.11 ms & per pass times of 1.3217 +- 0.0032 ms.
+Since each pass after the first only hits one 64B cache line per page this
+translates to about 1./64/1.3217e-3 =~ 11.82 GiB/s throughput.
+
+Of course, there is more going on than *only* memory transfer (not a lot more!),
+but this is also just one example benchmark.  `perf stat` (on Linux, anyway) may
+afford a more refined understanding of this kind of throughput.  Similarly, a
+smaller 2MiB file on a HugeTLB FS might eliminate all TLB misses to study L3 CPU
+cache bandwidth (or even L2 for some CPUs these days).[^3]  And so on.  Data
+moves around in many ways and data motion is often a bottleneck and `fread` is
+here usually just one piece of a bigger puzzle.
+
+[^1]: Or maybe by `cp somebigfile $j; truncate -s $((1024*1024*1024))`, etc.
+
+[^2]: On Linux anyway..
+
+[^3]: Measuring IO *itself* is also only one application of `fread`.  The
+original inspiration was eliminating IO time from *other* benchmarks.
--- fread.nim
+++ fread.nim
@@ -1,50 +1,49 @@
 when not declared(addFloat): import std/formatFloat
+import std/times, cligen/[mfile, mslice]
 when defined(windows):
-  import std/[winlean, times]
+  import std/winlean
   let sin = getStdHandle(STD_INPUT_HANDLE)
   proc read(fd: Handle, buf: pointer, len: int): int =
     let len = min(int32.high.int, len).int32
     var nRd: cint
     if readFile(fd, buf, len, nRd.addr, nil) == 0: -1 else: int(nRd)
-else:
-  import std/[posix, times]
+else: import std/posix
 
-proc fread*(bsz=65536, limit=0u64, verb=false, paths: seq[string]) =
+proc fread*(bsz=65536, lim=0i64, nPass=1, off=64, verb=false,paths:seq[string])=
   ## This is like `cat`, but just discards data.  Empty `paths` => just read
   ## from stdin.  That can be useful to ensure data is in an OS buffer cache
   ## or try to evict other data (more portably than /proc/sys/vm/drop_caches)
   ## for cold-cache runs, measure drive/pipe or device throughput, etc.  Eg. in
   ## Zsh you can say: `fread \*\*` or `fread -l $((1<<30)) < /dev/urandom`.
   ##
-  ## Users may pass paths to FIFOs/named pipes or other block-on-open special
-  ## files. We want to skip those but also avoid check-then-use races.  So, open
-  ## O_NONBLOCK, but then immediately fstat to skip non-regular & reactivate
-  ## blocking with fcntl to be CPU-sharing-friendly.
+  ## Users may pass paths to FIFOs/named pipes/other block-on-open special files
+  ## which are skipped.  Anything named is only used if mmap-able & only 1 byte
+  ## (really 1 cache line) per 4096 is used by the process.  Can use multiple
+  ## passes to measure DIMM bandwidth through a CPU prefetching lens.
   var buf = newString(bsz)
-  var n = 0u64
-  let mx = if limit != 0: limit else: uint64.high
+  var n = 0i64
+  let mx = if lim != 0: lim else: int64.high
   let t0 = if verb: epochTime() else: 0
-  when defined(windows):
-    if paths.len == 0:
-        while n < mx and (let k = read(sin, buf[0].addr, bsz); k > 0): inc n, k
+  var s = 0
+  if paths.len == 0:
+    when defined(windows):
+      while n < mx and (let k = read(sin, buf[0].addr, bsz); k > 0): inc n, k
+    else:
+      while n < mx and (let k = read(0, buf[0].addr, bsz); k > 0): inc n, k
   else:
-   if paths.len == 0:
-    while n < mx and (let k = read(0, buf[0].addr, bsz); k > 0): inc n, k
-   else:
-    var fd, flags: cint
-    var st: Stat
     for path in paths:
-      if (fd = open(path.cstring, O_RDONLY or O_NONBLOCK); fd >= 0) and
-          fstat(fd, st) == 0 and S_ISREG(st.st_mode) and
-          (flags = fcntl(fd, F_GETFL); flags >= 0) and
-          fcntl(fd, F_SETFL, flags and not O_NONBLOCK) == 0:
-        while n < mx and (let k = read(fd, buf[0].addr, bsz); k > 0): inc n, k
-      if fd >= 0 and fd.close < 0:
-        break
+      if (let mf = mopen path; mf.mem != nil):
+        for pass in 0..<nPass: # Use pass-scaled within page offset (line size)
+          for o in countup(pass*off and 4095, mf.len - 1, 4096):
+            inc s, mf.mslc[o.int].int
+            if o > mx: break
+          inc n, mf.mslc.len # Pass1: OS do VM page; Pass2+: CPU do 1 cache line
+        mf.close             # BUT above^can vary; Can measure via Lin.Regress.
   if verb:
     let dt = epochTime() - t0
-    echo "fread ", n, " bytes in ", dt, " sec = ", n.float/dt/1e9, " GB/s"
+    echo "fread ",n," bytes in ",dt," s: ",n.float/dt/1e9," GB/s par",s and 1
 
 when isMainModule: import cligen; dispatch fread, help={
-  "bsz": "buffer size for IO", "limit": "max bytes to read; 0=>unlimited",
-  "paths": "paths: paths to read in", "verb": "print bytes read"}
+  "bsz": "buffer size for stdin IO", "lim": "max bytes to read; 0=>unlimited",
+  "nPass": "passes per file", "off": "total [off*0-origin-pass within pages]",
+  "verb": "print bytes read", "paths": "paths: paths to read in"}
