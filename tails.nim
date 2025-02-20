d873e32552fc87d5feddf3f1f33e2dd328461406
Charles Blake <charlechaud@gmail.com>
2025-02-20 01:40:39 -0500

    Re-write state machine/buffering logic using a template in terms of conceptually simple passed templates get/put/copy/drop/divide/rest and just one cyclic buffer never using more than abs(tail) space.  This obsoletes --body & --repeat options which is a BREAKING CHANGE.
    Move imports near actual use site.
    
    In light of maybe symlinking head & tail, move `--sep` -> `--divide` so
    short option does not collide with GNU coreutils `-s,--sleep-interval`
    which is also a BREAKING CHANGE.
    
    Auto-add `eor` to `divider` early.
    
    Interpret `plusN` parse into head = 1-thatPlusTail as per "line number
    coordinates" derived conventions instead of simpler (to me, anyway)
    "amount of records".
    
    Call new headTail-template using doFilter proc and leave a note to
    also translate `-z`.  (Should probably templatize those w/args h,t.)
    
    Fix `--head` | `--tail` missing argument to error out & simplify
    `argHelp` return given `NRow` type (rather than a specialized `int`).
    
    Update help strings, documenting how negative values work and dropping
    two options obsoleted by new buffering logic.

--- tails.nim
+++ tails.nim
@@ -1,70 +1,60 @@
 when not declared(stdout): import std/syncio
 import std/[os, terminal, parseutils]; import std/strutils except parseInt
-include cligen/unsafeAddr
-from cligen/osUt import getDelims, urite, uriteBuffer, ureadBuffer, c_getdelim
-proc free(pointr: cstring) {.importc, header: "<stdlib.h>".}
 
-proc wrec(cs: cstring, n: int, eor: char): bool {.inline.} = # NOTE n-1
-  stdout.uriteBuffer(cs, n-1) < n-1 or stdout.uriteBuffer(eor.unsafeAddr, 1) < 1
+proc bump(j: var int, m: int) = (inc j; if j == m: j = 0) # Ring index helper
+template headTail*(R: type; get,put,copy,drop,rest; head, tail: int; divide) =
+  ## 1-pass, small mem Python-like b<0 [a:b] slices | their logical complements.
+  ## Here head<0 drops@start & tail<0 drops@end (swapped from GNU coreutils -n).
+  var r: R
+  for _ in 1 .. head.abs:           ##### MAYBE EMIT HEAD
+    if not r.get: r.drop; return        # Early end of input; Done
+    if head > 0: put r.cs, r            # Write lines|skip in emit|skip modes
+  if tail == 0:                         # Finish up no-TAIL case
+    if head < 0: rest r                 # Copy rest of file
+    r.drop; return                      # Done
+  let nTail = tail.abs              ##### MAYBE EMIT|BUFFER !TAIL
+  var buf = newSeq[string](nTail)       # Cyclic buf for TAIL
+  var i, j: int                         # Total (from EOHead) & Cyclic index
+  while r.get:                          # Get new record | EOF
+    if tail < 0 and i >= nTail:         # If clipping tail & have enough, emit..
+      put buf[j], r                     #..about to be clobbered non-tail saved.
+    copy buf[j], r                      # Save data & bump; Could become 1 big..
+    inc i; j.bump nTail                 #..buffer instead of nTail line bufs.
+  if tail > 0:                      ##### MAYBE EMIT TAIL
+    if head > 0 and i > tail: r.divide  # Non-contiguity detected! Show caller
+    if i <= tail: j = 0                 # Not wrapped => Reset
+    for _ in 1 .. min(i, tail):         # For however many after EOHead we got..
+      put buf[j], r; j.bump nTail       #..emit & cycle forward.
+  r.drop                                # Free input buffer
 
-proc bumpMod(j: var int; m: int): int {.inline.} = # circ.buffer helper
-  j.inc; if j >= m: j = 0
-  j
-
-proc outer(f: File; head,tail: int; ird,eor: char; sep="--",repeat=false): int =
-  var tBuf = newSeq[cstring](max(1, tail))
-  var tLen = newSeq[int](max(1, tail))
-  var room = newSeq[csize_t](max(1, tail))
-  var i, j: int
-  var wrote = false
-  while (let n = c_getdelim(tBuf[j].addr, room[j].addr, cint(ird), f);n)+1 > 0:
-    tLen[j] = n
-    if i < head:
-      if wrec(tBuf[j], tLen[j].int, eor): return 1 #Q: chk for disk full/ENOSPC?
-      wrote = true
-    elif i==head and tail <= 0: return  # no tail; done
-    discard j.bumpMod(tail)
-    i.inc
-  if i <= head and tail <= 0: return    # no tail; done
-  let j0 = j
-  if not repeat and head + tail > i:    # head & tail degenerate into `cat`
-    for it in 0 ..< head + tail - i:
-      discard j.bumpMod(tail)
-  while true:
-    if sep.len > 0 and wrote: stdout.write sep, eor; wrote = false
-    if tLen[j] > 0: (if wrec(tBuf[j], tLen[j].int, eor): return 1)
-    if tBuf[j] != nil: free(tBuf[j])
-    if j.bumpMod(tail) == j0:
-      break
-
-proc inner(f: File; head,tail: int; ird,eor: char): int =
-  var i = 0
-  if tail > 0:                          # Inefficient/high resource use case
-    var bBuf: string                    # O(input - (head+tail)) body buffer
-    var rows = newSeq[int](head + tail) # offset can be a circ.buf, though.
-    var j = 0
-    for (cs, n) in f.getDelims(ird):
-      if i >= head:
-        rows[j.bumpMod tail] = bBuf.len
-        bBuf.setLen bBuf.len + n
-        copyMem bBuf[^n].addr, cs, n - 1
-        bBuf[^1] = eor
-      i.inc
-    let n = if head + tail <= i: rows[j.bumpMod tail] else: 0
-    if bBuf.len > 0:
-      if stdout.uriteBuffer(bBuf[0].addr, n) < n: return 1
-  else:                                 # Very efficient/low resource case
-    for (cs, n) in f.getDelims:
-      if i >= head:
-        if wrec(cs, n, eor): return 1   # write head-th line and then IO loop
-        var buf = newString(65536)
-        while true: # Nixing stdio abstraction elims work;Or sendfile+splice
-          let n = f.ureadBuffer(buf[0].addr, buf.len)
-          if n > 0:
-            if stdout.uriteBuffer(buf[0].addr, n) < n: return 1
-          if n < buf.len:
-            break
-      i.inc
+from cligen/osUt import urite, uriteBuffer, ureadBuffer, c_getdelim
+proc doFilter(f: File; head,tail: int; ird,eor: char; divider="--"): bool =
+  proc free(pointr: cstring) {.importc, header: "stdlib.h".}
+  type Rec = tuple[cs: cstring; len: int; room: uint]
+  template get(r): untyped =
+    r.len = c_getdelim(r.cs.addr, r.room.addr, cint(ird), f)
+    r.len + 1 > 0
+  template copy(d, s) = d.setLen s.len; copyMem d[0].addr, s.cs, s.len - 1
+  template drop(r) = free(r.cs)
+  template put(b, r) =  # Buffer to write, Input buffer to drop on write error
+    let n = b.len - 1
+    if stdout.uriteBuffer(b[0].addr, n) < n or
+       stdout.uriteBuffer(eor.addr, 1) < 1:
+      r.drop; return true               # Write error; Return true
+  template rest(r) =    # Input buffer to drop on write error
+    var buf = newString(65536)
+    while true:         # Nixing stdio use can elim work; Also sendfile/splice?
+      let n = f.ureadBuffer(buf[0].addr, buf.len)
+      if n > 0:
+        if stdout.uriteBuffer(buf[0].addr, n) < n:
+          r.drop; return true           # Write error; Return true
+      if n < buf.len:
+        break
+  template divide(r) =
+    if divider.len > 0 and
+       stdout.uriteBuffer(divider[0].addr, divider.len) < divider.len:
+      r.drop; return true               # Write error; Return true
+  headTail Rec, get, put, copy, drop, rest, head, tail, divide
 
 type NKind = enum doN=0, plusN, fitN
 type NRow = object
@@ -77,13 +67,14 @@ proc nToFit(height, nH1, nH, nF: int): int =
   result = budget div nF
   if result*nF > budget: dec result
 
-proc tails(head=NRow(), tail=NRow(), follow=false, bytes=false, sep="--",
-           body=false, repeat=false, header="", quiet=false, verbose=false,
-           ird='\n', eor='\n', paths: seq[string]): int =
-  ## Emit|cut head|tail|both.  This combines & generalizes normal head/tail.
-  ## "/[n]" for `head|tail` infers a num.rows s.t. output for n files fits in
+proc tails(head=NRow(), tail=NRow(), follow=false, bytes=false, divide="--",
+           header="", quiet=false, verbose=false, ird='\n', eor='\n',
+           paths: seq[string]): int =
+  ## Unify & enhance normal head/tail to emit|cut head|tail|both.  "/[n]" for
+  ## `head|tail` infers a num.rows s.t. output for n files fits in
   ## ${LC_LINES:-${LINES:-ttyHeight}} rows. "/" alone infers that n=num.inputs.
   let paths = if paths.len > 0: paths else: @[""]
+  let divider = divide & $eor
   var head = head; var tail = tail
   let doHeaders = verbose or (not quiet and paths.len > 1)
   var hdr, hdr1: string; var nH, nH1: int
@@ -96,8 +87,9 @@ proc tails(head=NRow(), tail=NRow(), follow=false, bytes=false, sep="--",
                else: terminalHeight()
   if head.kind == fitN and head.n == 0: head.n = paths.len
   if tail.kind == fitN and tail.n == 0: tail.n = paths.len
+  elif tail.kind == plusN: head.n = 1-tail.n; tail.kind = doN; tail.n = 0
   if head.kind==fitN and tail.kind==fitN: # Approximate; Could be enhanced.
-    let nSep = sep.count(eor) + 1         # E.g., -h2 -t4 => 2x more hd than tl.
+    let nSep = divider.count(eor)         # E.g., -h2 -t4 => 2x more hd than tl.
     let n2 = height.nToFit(nH1+nSep, nH+nSep, max(head.n, tail.n))
     head.n = n2 div 2; tail.n = n2 div 2
   elif head.kind == fitN: head.n = height.nToFit(nH1, nH, head.n)
@@ -110,9 +102,8 @@ proc tails(head=NRow(), tail=NRow(), follow=false, bytes=false, sep="--",
         firstHeader = false
         stdout.urite hdr1%path
       else: stdout.urite hdr%path
-    var f = if path.len > 0: open(path) else: stdin
-    if body: (if f.inner(head.n, tail.n, ird, eor) != 0: return 1)
-    else   : (if f.outer(head.n, tail.n, ird, eor, sep, repeat) != 0: return 1)
+    let f = if path.len > 0: open(path) else: stdin
+    if f.doFilter(head.n, tail.n, ird, eor, divider): return 1
     if f != stdin: f.close
 
 when isMainModule:
@@ -132,12 +123,12 @@ when isMainModule:
     result.add cmdLine                  #..but such are super rare usage; POSIX
     if cn == "head":                    #..`head` literally only has -n option. 
       for clp in mitems result:
-        if clp == "--": break   
+        if clp == "--": break           # Also translate -z -> -i\0 \e\0
         if   clp.startsWith("-n"): clp = "-h"  & clp[2..^1]
         elif clp.startsWith("-c"): clp = "-ch" & clp[2..^1]
     elif cn == "tail":
       for clp in mitems result:
-        if clp == "--": break   
+        if clp == "--": break           # Also translate -z -> -i\0 \e\0  
         if   clp.startsWith("-n"): clp = "-t"  & clp[2..^1]
         elif clp.startsWith("-c"): clp = "-ct" & clp[2..^1]
 
@@ -153,22 +144,22 @@ when isMainModule:
       if s.len == 1 or parseInt(s, dst.n) < s.len:
         a.msg = "Bad value: \"$1\" for \"$2\"; expecting \"+INTEGER\"\n$3" %
                 [a.val, a.key, a.help]; return false
-    elif parseInt(s, dst.n) < s.len:
+    elif s.len == 0 or parseInt(s, dst.n) < s.len:
       a.msg = "Bad value: \"$1\" for option \"$2\"; expecting int\n$3" %
               [a.val, a.key, a.help]; return false
     return true
+
   proc argHelp*(defVal: NRow, a: var ArgcvtParams): seq[string] =
-    @[a.argKeys, if a.parNm in ["head","tail"]: "int|/[n]" else: "int", "0"]
+    @[a.argKeys, "int|/[n]", "0"]
 
   dispatch tails, short={"help": '?', "bytes": 'c', "header": 'H'}, help={
     "paths"  : "[paths: string...; '' => stdin]",
-    "head"   : "number of rows at the start",   # TODO <0
-    "tail"   : "number of rows at the end",     # TODO <0
-    "follow" : "CLIGEN-NOHELP", # --follow & --bytes are To Be Done
+    "head"   : ">0 emit | <0 cut this many @start",
+    "tail"   : ">0 emit | <0 cut this many @end;\n" &
+               "Leading \"+\" => `head` = 1 - THIS.",
     "bytes"  : "CLIGEN-NOHELP", # units of `head` & `tail` are bytes
-    "sep"    : "separator, for non-contiguous case",
-    "body"   : "body not early/late tail",
-    "repeat" : "repeat rows when head + tail >= n",
+    "follow" : "CLIGEN-NOHELP", # --follow & --bytes are To Be Done
+    "divide" : "separator, for non-contiguous case",
     "header" : "header format; \"\" => \\n==> $1 <==\\n",
     "quiet"  : "never print file name headers", # --silent alias?
     "verbose": "always print file name headers",
