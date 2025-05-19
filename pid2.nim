2bb6b2c47b1debcedb3732690659eabf451586bd
Charles Blake <charlechaud@gmail.com>
2025-05-19 11:41:11 -0400

    Allow affinity set to user-chosen CPU for outer/shell-level loops, e.g. `for k in `seq 0 "$((j-1))"`; do pid2 "$@" "$k" & done; wait`.

--- pid2.nim
+++ pid2.nim
@@ -3,9 +3,12 @@ import std/[os, posix, parseutils]
 var pid  = getpid()
 var last = 0.Pid
 var t    = 300  # Linux starts Process ID table @300
+var cpu  = 1
 var xSt: cint
 if paramCount()>0 and parseInt(1.paramStr,t)!=1.paramStr.len:
   quit "Expecting integer to try to wrap PID table to", 1
+if paramCount()>1 and parseInt(2.paramStr,cpu)!=2.paramStr.len:
+  quit "Expecting CPU to set affinity for", 2
 let tgt = t.Pid
 
 template nextPid =
@@ -17,7 +20,7 @@ template nextPid =
   else : discard waitpid(pid, xSt, 0) # parent=>wait
 
 when defined(linux):
-  import cligen/osUt;setAffinity([1.cint]) # Alder 14X faster
+  import cligen/osUt;setAffinity([cpu.cint]) # Alder 14X faster
 
 while pid > tgt and last < pid: nextPid() # Get (last>=pid)
 while pid < tgt: nextPid()                # 1st free past tgt
