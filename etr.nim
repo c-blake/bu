f5cc0ed86012cba7ec2962e57d06a5c9a17bb159
Charles Blake <charlechaud@gmail.com>
2025-09-14 09:22:06 -0400

    Simplify in several ways:   - only use one estimator for rate, not also rate variation   - have `$` do less calculation - etc() should do almost all   - meaning ETR structure moves from rateLo,rateHi -> leftLo,leftHi   - rename some parameter & var names to be simpler & more consistent     (`did2` -> `did`, `r` -> `e`, )   - Use fixed width, 0-pad format for %done & re-order columns to have     more text-width variable "left" to the right of stable width cols.     This lets one better visually track the convergence to final time.   - Re-purpose `scale` param as a range of probabilities for quantiles   - Simplify measurement loop logic a lot in light of one estimator

--- etr.nim
+++ etr.nim
@@ -1,14 +1,12 @@
-import std/[times, strformat, osproc, strutils, os, posix, algorithm, math],
+import std/[times, strformat, osproc, strutils, os, posix, math],
        cligen, cligen/[sysUt, osUt, humanUt], adix/[xhist1, lna, embist]
 when not declared(readFile): import std/[syncio, formatfloat]
 
 var wOL, wOS = 0.9375 #TODO Generalize to (EW|LW|x)M[AD]|Qtls for rate location
-xhist1.def LDist, lna, exp, EMBist[float], true, wOL # & scale estimate chosen
-xhist1.def SDist, lna, exp, EMBist[float], true, wOS # at run-time by CLI.
-proc initLDist(hl: string): LDist = wOL = 2.0^(-1/parseFloat(hl)); result.init
-proc initSDist(hl: string): SDist = wOS = 2.0^(-1/parseFloat(hl)); result.init
+xhist1.def LDist, lna, exp, EMBist[float], true, wOL
+proc initRDist(hl: string): LDist = wOL = 2.0^(-1/parseFloat(hl)); result.init
 
-type ETR* = tuple[done,total, rateLo,rateMid,rateHi: float; etc: DateTime]
+type ETR* = tuple[done,total, leftLo,rateMid,leftHi: float; etc: DateTime]
 
 var done0, done1, rate0, rate1, left0, left1, etc0, etc1, ratio0, ratio1 = ""
 proc parseColor(color: seq[string]) =
@@ -21,35 +19,33 @@ proc parseColor(color: seq[string]) =
     of "done1" : done1  = v
     of "rate0" : rate0  = v
     of "rate1" : rate1  = v
-    of "left0" : left0  = v
-    of "left1" : left1  = v
     of "etc0"  : etc0   = v
     of "etc1"  : etc1   = v
+    of "left0" : left0  = v
+    of "left1" : left1  = v
     of "ratio0": ratio0 = v
     of "ratio1": ratio1 = v
     else: Value !! "bad color line: \"" & spec & "\""
 
-proc etc*(t0: DateTime; total,did2, rateLo,rateMid,rateHi: float): ETR =
+proc etc*(t0: DateTime; total, did, rateLo, rateMid, rateHi: float): ETR =
   let invTot     = if total != 0: 1/total else: 1
   result.total   = total
-  result.done    = did2*invTot
-  result.rateLo  = rateLo*invTot
+  result.done    = did*invTot
   result.rateMid = rateMid*invTot
-  result.rateHi  = rateHi*invTot
-  let leftMid    = (total - did2)/rateMid
+  result.leftLo  = (total - did)/rateHi
+  let leftMid    = (total - did)/rateMid
+  result.leftHi  = (total - did)/rateLo
   result.etc     = t0 + initDuration(milliseconds=int(leftMid*1000))
 
-proc `$`*(r: ETR): string =
-  let et = ($r.etc)[4..^7].replace("-", "")     # Strip year, zone & date '-'s
-  let (leftLo, leftHi) = ((1 - r.done)/r.rateHi, (1 - r.done)/r.rateLo)
-  let absRate = r.rateMid*r.total
-  &"{done0}{1e2*r.done:.3f} %{done1} {rate0}{1e4*r.rateMid:.2f} bp/s{rate1} " &
-  &"{left0}{leftLo:.1f} - {leftHi:.1f} sLeft{left1} " &
-  &"{rate0}{absRate:.4g} /s{rate1} {etc0}{et}{etc1}"
+proc `$`*(e: ETR): string =
+  let et = ($e.etc)[4..^7].replace("-", "")     # Strip year, zone & date '-'s
+  &"{done0}{1e2*e.done:06.3f} %{done1} " &
+  &"{rate0}{1e4*e.rateMid:.2f} bp/s {e.rateMid*e.total:.4g} /s{rate1} " &
+  &"{etc0}{et}{etc1} {left0}{e.leftLo:.1f} - {e.leftHi:.1f} sLeft{left1}"
 
-proc expSize(r: ETR; osz, relTo: float): string =
-  if relTo == 1.0: $int(osz.float/r.done) & " B"
-  else: &"{osz.float/r.done/relTo.float:.3f}"
+proc expSize(e: ETR; osz, relTo: float): string =
+  if relTo == 1.0: $int(osz.float/e.done) & " B"
+  else: &"{osz.float/e.done/relTo.float:.3f}"
 
 func notInt(str: string): bool =
   for ch in str: (if ch notin {'-','0'..'9'}: return true)
@@ -64,21 +60,21 @@ proc processAge(pfs: string): float =
   let start = (pfs & "/stat").readFile.split[21].parseInt
   float(uptime - start)*0.01    # Jiffies -> seconds
 
-proc wrStatus(r: ETR; outp,pfs,relTo: string; tot,estMin,RatMin: float): int =
+proc wrStatus(e: ETR; outp,pfs,relTo: string; tot,estMin,RatMin: float): int =
   if outp.len > 0:
     let osz = if outp.notInt: outp.execProcess.strip.parseFloat
               else: (try: (pfs & "fd/" & outp).getFileSize.float except: 0.0)
     let rTo = if relTo.len<1:1.0 else: (try: relTo.strip.parseFloat except:(try:
                 relTo.execProcess.strip.parseFloat except:
                   erru relTo, " output did not parse as a float\n"; 1.0))
-    echo r,&" {ratio0}",r.expSize(osz, if rTo > 0.0: rTo else: tot),&"{ratio1}"
-    result = if r.done>=estMin and osz>RatMin*r.done*(if rTo>0:rTo else:tot): 2
+    echo e,&" {ratio0}",e.expSize(osz, if rTo > 0.0: rTo else: tot),&"{ratio1}"
+    result = if e.done>=estMin and osz>RatMin*e.done*(if rTo>0:rTo else:tot): 2
              else: 0
-  else: echo r
+  else: echo e
 
 proc etr*(pid=0, did="", total="", age="", ageScl=1.0, measure=0.0, outp="",
-          relTo="", RatMin=1e17, estMin=0.0, kill="NIL", locus="10", scale="30",
-          colors: seq[string] = @[], color: seq[string] = @[]): int =
+    relTo="", RatMin=1e17, estMin=0.0, kill="NIL", locus="9", scale=0.1..0.9,
+    update=false, colors: seq[string] = @[], color: seq[string] = @[]): int =
   ## Estimate Time Remaining (ETR) using A) work already done given by `did`,
   ## B) expected total work as given by the output of `total`, and C) the age of
   ## processing (age of `pid` or that produced by the `age` command).  Commands
@@ -98,42 +94,36 @@ proc etr*(pid=0, did="", total="", age="", ageScl=1.0, measure=0.0, outp="",
   ## Exit status is 2 if *output:input > RatMin* after `estMin` progress.
   if did.len == 0:
     erru "Need @least `did` &likely `pid`; --help says more\n";Parse!!""
-  let pfs  = "/proc/" & $pid & "/"
-  let age  = if pid==0 or age.len>0: age.execProcess.strip.parseFloat*ageScl
-             else: pfs.processAge
-  let tot  = if total.len == 0: getFileInfo(pfs & "fd/" & did).size.float
-             elif total.notInt: total.execProcess.strip.parseFloat
-             else: getFileInfo(pfs & "fd/" & total).size.float
-  template q: untyped = (if did.notInt: did.execProcess.strip.parseFloat
-                         else: readFile(pfs&"fdinfo/"&did).split[1].parseFloat)
+  let pfs = &"/proc/{pid}/"
+  let age = if pid==0 or age.len>0: age.execProcess.strip.parseFloat*ageScl
+            else: pfs.processAge
+  template qT:untyped=(if total.len==0: getFileInfo(&"{pfs}fd/{did}").size.float
+                       elif total.notInt: total.execProcess.strip.parseFloat
+                       else: getFileInfo(&"{pfs}fd/{total}").size.float)
+  template qD:untyped=(if did.notInt: did.execProcess.strip.parseFloat
+                       else: readFile(&"{pfs}fdinfo/{did}").split[1].parseFloat)
   color.parseColor
   try:
-    var did1 = q; var t = now() # 1 data point affords only a trivial estimate
-    let r = did1/age            #.. for rate, namely `did1/age`.
-    template writeStatusMaybeKill(et) =
-      result = wrStatus(et, outp, pfs, relTo, tot, estMin, RatMin)
+    var tot  = qT
+    var did0 = qD; var t0=now()  # 1 data point affords only a trivial estimate
+    let r0   = did0/age          #.. for rate, namely `did0/age`.
+    template writeStatusMaybeKill(e) =
+      result = wrStatus(e, outp, pfs, relTo, tot, estMin, RatMin)
       if result != 0:           # outp given,yet Osz/Isz getting too big
         discard execCmd(&"{kill} {pid}");quit 2 # => Kill & Quit.
-    writeStatusMaybeKill etc(t, tot, did1, r, r, r)
+    writeStatusMaybeKill etc(t0, tot, did0, r0, r0, r0)
     if measure > 0:             # -m also could stand for "monitor"
-      let dt = int(measure*1e3) # Nim sleep takes millisec
-      var tms = @[t.toTime.toUnixFloat - age, t.toTime.toUnixFloat]
-      var dids = @[0.0, did1] #TODO `tot` MAY be dyn,but reQry wasteful if not
-      var loc = initLDist(locus)
-      loc.add did1/age
+      let ms = int(measure*1e3) # Nim sleep takes millisec
+      var rate = initRDist(locus)
+      rate.add r0
       while pfs.dirExists:      # Re-query time to trust sleep dt less..
-        sleep dt; dids.add q    #..in case we get SIGSTOP'd or etc.
-        let t = now()
-        tms.add t.toTime.toUnixFloat
-        let rate = (dids[^1] - dids[^2])/(tms[^1] - tms[^2])
-        loc.add rate
-        let rateMid = loc.quantile 0.5
-        let leftMid = (tot - dids[^1])/rateMid
-        var scl = initSDist(scale)      # N sec left=>want data from past N sec.
-        let i0 = lowerBound(tms, tms[^1] - leftMid) # May wax|wane=>Start anew.
-        for i in i0..<tms.len-1: scl.add (dids[i+1]-dids[i])/(tms[i+1]-tms[i])
-        let (rateLo, rateHi) = (scl.quantile 0.15, scl.quantile 1-0.15)
-        writeStatusMaybeKill etc(t, tot, dids[^1], rateLo, rateMid, rateHi)
+        sleep ms                #..in case we get SIGSTOP'd or etc.
+        let did1 = qD; let t = now()
+        rate.add (did1 - did0)/(t - t0).inNanoseconds.float*1e9
+        did0 = did1; t0 = t
+        if update: tot = qT
+        writeStatusMaybeKill etc(t, tot, did1, rate.quantile scale.a,
+                                 rate.quantile 0.5, rate.quantile scale.b)
   except IOError: quit 3        # Probably some racy `readFile` failure
 
 when isMainModule: include cligen/mergeCfgEnv; dispatch etr,
@@ -150,7 +140,8 @@ when isMainModule: include cligen/mergeCfgEnv; dispatch etr,
         "estMin" : "require > this much progress for RatMin",
         "kill"   : "run this cmd w/arg `pid` if ratio test fails",
         "locus"  : "maker for moving rate location (for ETC)",
-        "scale"  : "maker for moving rate scale (for range)",
+        "scale"  : "lo,hi probabilities for rate/etc range",
+        "update" : "re-query total each sample in measure mode",
         "colors" : "color aliases; Syntax: name = ATTR1 ATTR2..",
         "color"  : "text attrs for syntax elts; Like lc/etc."},
   short={"ageScl": 'A'}
