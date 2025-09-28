2daccb347565bcd4127da03710a3dabb3bc55d9a
Charles Blake <charlechaud@gmail.com>
2025-09-28 14:30:00 -0400

    Fix bug where `yO -= h` could yield yO < 0 w/max; Fix PgDn fencepost Simplify dec -> assignment.

--- vip.nim
+++ vip.nim
@@ -272,7 +272,7 @@ proc tui(alt=false, d=5): int =    # 9) MAIN TERMINAL USER-INTERFACE
       else: put1 "", "No Room For Help"
       discard iK.getKey
     elif doPicks:
-      yO = max(yO, pick - h + 1)
+      yO = max(0, yO); yO = max(yO, pick - h + 1)
       nIt = putN(yO, pick)
     putp carriage_return
     let jCtot = hdr.printedLen + q[0..<jC].printedLen # right_cursor treats 0 as
@@ -287,10 +287,10 @@ proc tui(alt=false, d=5): int =    # 9) MAIN TERMINAL USER-INTERFACE
                 return its.len - 1) else: return - 1)
     of CtrlC:  return -1                # & below exit-like suspend
     of CtrlZ:  tRestore alt; discard kill(getpid(), SIGTSTP); tInit alt
-    of LineUp: (if pick > 0      : (dec pick; if pick <  yO    : dec yO)) #XXX okx loop
+    of LineUp: (if pick > 0      : (dec pick; if pick <  yO    : yO = pick)) #XXX okx loop
     of LineDn: (if pick < nIt - 1: (inc pick; if pick >= yO + h: inc yO)) #XXX okx loop
     of PgUp:   (if pick > h      : (yO -= h; pick -= h) else: (yO=0; pick=0))
-    of PgDn:   (if pick < nIt - h: (yO += h; pick += h) else: pick = nIt - 1)
+    of PgDn:   (if pick < nIt-h-1: (yO += h; pick += h) else: pick = nIt - 1)
     of Home:   yO = 0; pick = 0
     of End:    (if nIt > 0: pick = nIt - 1) # Last List Navigation
     of CtrlA:  jC = 0                   # Qry Bulk NavEdit: Start,End,Right,Left
