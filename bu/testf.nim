bad83c7f010c19acfc1310c8f0dc191a9ea928ab
Charles Blake <charlechaud@gmail.com>
2025-09-29 03:27:58 -0400

    Remove caching system as near-future `vip` will be doing this itself.

--- bu/testf.nim
+++ bu/testf.nim
@@ -1,27 +1,30 @@
-import std/[posix, tables], cligen/mslice # For `dirs|vip -k libtestf.so:cdable`
+##[ `vip` caches these answers for UI logic simplicity & efficiency.  Stale info
+can be displayed if FS changes fast relative to interactive pick sessions. ]##
 
-# Using temporally maybe stale caching is probably wanted if interactive pick
-# sessions are short-lived relative to filesystem dynamics.
-var cdableCache: Table[MSlice, bool]    # CALLER MUST NEVER RELOCATE BACKING MEM
-                                        # (`vip` does not after `parseIn`.)
-var buf = ""                            # Reused path buffer to be NUL/\0-term
+import std/posix        # .so source for use in `dirs|vip -k libtestf.so:cdable`
+
+var cpath = ""                            # Reused path buffer to be NUL/\0-term
 proc cdable(path: pointer, nPath: clong): cint {.noconv, exportc, dynlib.} =
-  let ms = MSlice(mem: path, len: nPath)
-  try: cdableCache[ms].cint             # Try cache & then `stat` first since ..
-  except:                               #..most likely failure is vanishing & ..
-    var st: Stat                        #..system more likely optimizes `stat`.
-    buf.setLen nPath; copyMem buf[0].addr, path, nPath
-    let cpath = cast[cstring](buf[0].addr)
-    let res = stat(cpath, st) == 0 and st.st_mode.S_ISDIR and
-              access(cpath, X_OK) == 0  # Net FS/ACLs/etc.=> `access` to confirm
-    cdableCache[ms] = res
-    res.cint
-# This module could grow a large family of `bu/ft`-like tests.  PRs welcome if
-# this would help your specific application setting.
+  if nPath == 0 or path.isNil:
+    return 0
+  cpath.setLen nPath    # `vip` does not open any files post `parseIn()`
+  copyMem cpath[0].addr, path, nPath
+  cint(chdir(cast[cstring](cpath[0].addr)) == 0)
 
-# On systems with nice enough terminal interaction for `vip` to make sense but
-# poor dynamically loadable lib support (are there any??), `vip` *could* add a
-# `--validation-coprocess=foo` to make a kid process to delegate requests to,
-# mostly blocked read, but ready to read a path, do whatever user-program tests
-# & write 1-byte.  While still 2 syscalls per request (in both parent & kid +
-# whatever validation work), they are at least fast-ish pipe-IO calls.
+#[ To cursor down, `vip` must test one at a time until a success.  To get more
+async/scalable needs a batch interface with forked kids which an ok idea since a
+hanging NFS mount can hang a kid process & we might want to kill it.  Laziness
+of outer validation may mean only 0..3 timeouts in any given UI interaction.
+So, they could be made 50..100ms.  There may be a way to build a critbit tree,
+monitor /proc/mounts, and only time out once per mount prefix or etc.  Of
+course, a file system could also come back online at any moment as well.
+
+On systems with nice enough terminal interaction for `vip` to make sense but
+poor dynamically loadable lib support (are there any??), `vip` *could* add a
+`--validation-coprocess=foo` to make a kid process to delegate requests to,
+mostly blocked on IPC read, but ready to read a path, do whatever user-program
+tests & write 1-byte.  While 2 syscalls per request (in both parent & kid +
+whatever validation work), they are at least fast-ish pipe-IO calls.
+
+This module could grow a large family of `bu/ft`-like tests.  PRs welcome if
+this would help your specific application setting. ]#
